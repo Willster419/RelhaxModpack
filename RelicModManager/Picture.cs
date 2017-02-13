e6bc060fbf3581a0d58af86ce7b13196713e7165
Willster419 <willster419@outlook.com>
2017-02-12 23:05:24 -0500

    settings up on 2/12 for work without IDE

---
+++ RelicModManager/Picture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RelicModManager
+{
+    public class Picture
+    {
+        public string name { get; set; }
+        public string URL { get; set; }
+        public Picture()
+        {
+
+        }
+        public Picture(string newName, string newURL)
+        {
+            name = newName;
+            URL = newURL;
+        }
+    }
+}
