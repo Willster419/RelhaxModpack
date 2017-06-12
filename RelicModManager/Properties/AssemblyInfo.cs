c2407daa37575e1f4009d908f86cbf15798d75db
Willster419 <willster419@outlook.com>
2017-06-12 08:10:19 -0400

    Version 23.1.3: -Fixed bugs with the xvm patcher

--- RelicModManager/Properties/AssemblyInfo.cs
+++ RelicModManager/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.23.1.2")]
-[assembly: AssemblyFileVersion("0.23.1.2")]
+[assembly: AssemblyVersion("0.23.1.3")]
+[assembly: AssemblyFileVersion("0.23.1.3")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
