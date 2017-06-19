6c39f155c9792ba22975738cf04b34ea15b939c7
Willster419 <willster419@outlook.com>
2017-06-19 19:46:44 -0400

    Version 23.2.1: -Updated autosize parameters in the main window -Moved restoring of user files to right after extraction, before patching, for user data files that are also the config files for mods. -Updated Translations

--- RelicModManager/MainWindow.cs
+++ RelicModManager/MainWindow.cs
@@ -26,8 +26,8 @@ namespace RelhaxModpack
         private WebClient downloader = new WebClient();
         private string tempPath = Path.GetTempPath();//C:/users/userName/appdata/local/temp
         private const int MBDivisor = 1048576;
-        private string managerVersion = "version 23.2.0";
-        private string today = "06/17/2017";
+        private string managerVersion = "version 23.2.1";
+        private string today = "06/19/2017";
         private string tanksLocation;//sample:  c:/games/World_of_Tanks
         //queue for downloading mods
         private List<DownloadItem> downloadQueue;
