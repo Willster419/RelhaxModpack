bde6040977273944a936266695b352333187c454
Willster419 <willster419@outlook.com>
2017-07-11 17:42:30 -0400

    Added new back-end tool: DatabaseListGenerater. It generates a tab separated csv style file that contains all mods and configs currently listed in the database.

---
+++ RelicModManager/DatabaseListGenerater.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RelhaxModpack
+{
+    public partial class DatabaseListGenerater : Form
+    {
+        //required class variables
+        private StringBuilder sb = new StringBuilder();
+        private int index = 0;
+        private string category = "";
+        private int level = 0;
+        private string modName = "";
+        private string configname = "N/A";
+        private string zipfile = "";
+        private bool enabled = false;
+        private List<Dependency> globalDependencies = new List<Dependency>();
+        private List<Category> parsedCatagoryList = new List<Category>();
+        private string header = "Index\tCategory\tMod\tConfig\tLevel\tZip\tEnabled";
+
+        public DatabaseListGenerater()
+        {
+            InitializeComponent();
+        }
+
+        private void DatabaseListGenerater_Load(object sender, EventArgs e)
+        {
+            //font scaling
+            this.AutoScaleMode = Settings.appScalingMode;
+            this.Font = Settings.appFont;
+            if (Settings.appScalingMode == System.Windows.Forms.AutoScaleMode.Dpi)
+            {
+                this.Scale(new System.Drawing.SizeF(Settings.scaleSize, Settings.scaleSize));
+            }
+            SaveSpreadsheetFileDialog.InitialDirectory = Application.StartupPath;
+            LoadDatabaseFileDialog.InitialDirectory = Application.StartupPath;
+        }
+
+        private void LoadDatabaseButton_Click(object sender, EventArgs e)
+        {
+            if (LoadDatabaseFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.Cancel)
+                return;
+            DatabaseLocation.Text = LoadDatabaseFileDialog.FileName;
+            //except actually load it here
+            //load database
+            globalDependencies = new List<Dependency>();
+            parsedCatagoryList = new List<Category>();
+            Utils.createModStructure2(LoadDatabaseFileDialog.FileName, true, globalDependencies, parsedCatagoryList);
+            if(!SpreadsheetLocation.Text.Equals(""))
+                SpreadsheetLocation.Text = "(old) " + SpreadsheetLocation.Text;
+        }
+
+        private void GenretateSpreadsheetButton_Click(object sender, EventArgs e)
+        {
+            //reset everything
+            sb = new StringBuilder();
+            index = 0;
+            category = "";
+            level = 0;
+            modName = "";
+            configname = "N/A";
+            zipfile = "";
+            enabled = false;
+            //ask where to save the file
+            if (SaveSpreadsheetFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.Cancel)
+                return;
+            //save it
+            sb.Append(header + "\n");
+            foreach (Category cat in parsedCatagoryList)
+            {
+                category = cat.name;
+                foreach (Mod m in cat.mods)
+                {
+                    index = m.index;
+                    level = 1;
+                    modName = m.name;
+                    configname = "N/A";
+                    zipfile = m.zipFile;
+                    enabled = m.enabled;
+                    //header = "Index,Category,Mod,Config,Level,Zip,Enabled";
+                    sb.Append(index + "\t" + category + "\t" + modName + "\t" + configname + "\t" + level + "\t" + zipfile + "\t" + enabled + "\n");
+                    if (m.configs.Count > 0)
+                        processConfigsSpreadsheetGenerate(m.configs, level + 1);
+                }
+            }
+            try
+            {
+                File.WriteAllText(SaveSpreadsheetFileDialog.FileName, sb.ToString());
+                SpreadsheetLocation.Text = "Saved in " + SaveSpreadsheetFileDialog.FileName;
+            }
+            catch (IOException ex)
+            {
+                SpreadsheetLocation.Text = "Failed to save in " + SaveSpreadsheetFileDialog.FileName + " (IOException, probably file open in another window)";
+            }
+            
+        }
+        private void processConfigsSpreadsheetGenerate(List<Config> configList, int newLevel)
+        {
+            //level++;
+            foreach (Config con in configList)
+            {
+                index = con.index;
+                configname = con.name;
+                zipfile = con.zipFile;
+                enabled = con.enabled;
+                //header = "Index,Category,Mod,Config,Level,Zip,Enabled";
+                sb.Append(index + "\t" + category + "\t" + modName + "\t" + configname + "\t" + newLevel + "\t" + zipfile + "\t" + enabled + "\n");
+                if (con.configs.Count > 0)
+                    processConfigsSpreadsheetGenerate(con.configs, newLevel + 1);
+                //else
+                    //level--;
+            }
+        }
+    }
+}
--- RelicModManager/Program.cs
+++ RelicModManager/Program.cs
@@ -32,40 +32,54 @@ namespace RelhaxModpack
                 //check what type of arg each one is
                 if (Regex.IsMatch(commandArgs[i], @"test$"))
                 {
+                    Utils.appendToLog("/test detected, loading in test mode");
                     testMode = true;
                 }
                 else if (Regex.IsMatch(commandArgs[i], @"skip-update$"))
                 {
+                    Utils.appendToLog("/skip-update detected, skipping update of application");
                     skipUpdate = true;
                 }
                 else if (Regex.IsMatch(commandArgs[i], @"patchday$"))
                 {
+                    Utils.appendToLog("/patchday detected, welcome database manager");
                     patchDayTest = true;
                 }
                 else if (Regex.IsMatch(commandArgs[i], @"auto-install$"))
                 {
+                    Utils.appendToLog("/auto-install detected");
                     autoInstall = true;
                     //parse the config file and advance the counter
                     configName = commandArgs[++i];
                 }
                 else if (Regex.IsMatch(commandArgs[i], @"crccheck2$"))
                 {
+                    Utils.appendToLog("/crccheck2 detected, why are you using this??");
                     Application.Run(new CRCCHECK2());
                     return;
                 }
                 else if (Regex.IsMatch(commandArgs[i], @"crccheck$"))
                 {
+                    Utils.appendToLog("/crccheck detected, why are you using this???");
                     Application.Run(new CRCCheck());
                     return;
                 }
                 else if (Regex.IsMatch(commandArgs[i], @"patchcheck$"))
                 {
+                    Utils.appendToLog("/patchcheck detected, loading in patch design mode");
                     Application.Run(new PatchTester());
                     return;
                 }
                 else if (Regex.IsMatch(commandArgs[i], @"databaseupdate$"))
                 {
+                    Utils.appendToLog("/databaseupdate detected, loading in database update mode");
                     Application.Run(new CRCFileSizeUpdate());
+                    return;
+                }
+                else if (Regex.IsMatch(commandArgs[i], @"databaseoutput$"))
+                {
+                    Utils.appendToLog("/databaseoutput detected, loading in database output mode");
+                    Application.Run(new DatabaseListGenerater());
                     return;
                 }
             }
