458bd61038d714ed1f0e55002167cf43bd8f1347
Grumpelumpf <grumpelumpf@gmail.com>
2017-07-17 22:31:40 +0200

    fixed finally the calculated size of Mods in the ModSelectionList updated german translation

--- RelicModManager/Translations.cs
+++ RelicModManager/Translations.cs
@@ -918,7 +918,7 @@ namespace RelhaxModpack
 
             //Section saveConfig
             english.Add("selectWhereToSave", "Select where to save user prefs");
-            german.Add("selectWhereToSave", "Das Modpack kann nicht aus dem World_of_Tanks Verzeichnis laufen. Bitte verschiebe die Anwendung in ein anderes Verzeichnis und versuchen Sie es erneut.");
+            german.Add("selectWhereToSave", "Bitte wähle wo die Speicherung erfolgen soll");
             polish.Add("selectWhereToSave", "Wybierz miejsce zapisywania prefiksów użytkownika");
         }
     }
