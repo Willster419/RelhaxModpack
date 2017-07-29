9be3e3d2093272b22d94d677791cf8dffdfd3820
Willster419 <willster419@outlook.com>
2017-07-29 12:49:25 -0400

    Added framework for re-writing the dependency logic

---
+++ RelicModManager/DatabaseLogic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RelhaxModpack
+{
+    public class DatabaseLogic
+    {
+        public string PackageName { get; set; }
+        public bool NotFlag { get; set; }
+    }
+}
