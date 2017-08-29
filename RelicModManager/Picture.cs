681364289720e492e71a3141693ab3c421048593
Willster419 <willster419@outlook.com>
2017-08-29 02:54:21 -0400

    started work on changing the picture preview to also be a video preview

--- RelicModManager/Picture.cs
+++ RelicModManager/Picture.cs
@@ -1,18 +1,28 @@
 ﻿
 namespace RelhaxModpack
 {
+    public enum MediaType
+    {
+        picture = 1,
+        youtube = 2
+    }
     public class Picture
     {
         //two-part variable. specifies wether it's part of a mod, or a mod's config
         //and the mod/config name
         public string name { get; set; }
+        //SAMPLE YOUTUBE URL: https://www.youtube.com/v/ZwY2E0hjGuU?version=3&autoplay=1
+        //http and https both work
         public string URL { get; set; }
+        //media type, 1 = picture (default), 2 = youtube video
+        public MediaType mediaType { get; set; }
         //constructor to setup the picture with the "name" and
         //the URL where the picture is located
         public Picture(string newName, string newURL)
         {
             name = newName;
             URL = newURL;
+            mediaType = MediaType.picture;
         }
         //sorts the mods
         public static int ComparePictures(Picture x, Picture y)
