f4fd1b97c4513d307bd099729cbcb9cdaf54ac43
Grumpelumpf <grumpelumpf@gmail.com>
2017-10-02 22:21:29 +0200

    added function CompareVersions()

--- RelicModManager/Utils.cs
+++ RelicModManager/Utils.cs
@@ -4387,5 +4387,22 @@ namespace RelhaxModpack
                 Utils.exceptionLog("DirectoryDelete", "Folder=" + folderPath, ex);
             }
         }
+
+        // https://stackoverflow.com/questions/30494/compare-version-identifiers
+        /// <summary>
+        /// Compare versions of form "1,2,3,4" or "1.2.3.4". Throws FormatException
+        /// in case of invalid version.
+        /// </summary>
+        /// <param name="strA">the first version</param>
+        /// <param name="strB">the second version</param>
+        /// <returns>less than zero if strA is less than strB, equal to zero if
+        /// strA equals strB, and greater than zero if strA is greater than strB</returns>
+        public static int CompareVersions(String strA, String strB)
+        {
+            Version vA = new Version(strA.Replace(",", "."));
+            Version vB = new Version(strB.Replace(",", "."));
+
+            return vA.CompareTo(vB);
+        }
     }
 }
