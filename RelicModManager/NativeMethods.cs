489f1c95e54cf05e0bffc7bea7a6dc5cfafbc904
Grumpelumpf <grumpelumpf@gmail.com>
2017-10-15 23:06:09 +0200

    added to get correct screen resolution of currently used screen for MainWindow

---
+++ RelicModManager/NativeMethods.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace RelhaxModpack
+{
+    public static class NativeMethods                         // https://stackoverflow.com/questions/254197/how-can-i-get-the-active-screen-dimensions
+    {
+        public const Int32 MONITOR_DEFAULTTOPRIMERTY = 0x00000001;
+        public const Int32 MONITOR_DEFAULTTONEAREST = 0x00000002;
+
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr MonitorFromWindow(IntPtr handle, Int32 flags);
+
+
+        [DllImport("user32.dll")]
+        public static extern Boolean GetMonitorInfo(IntPtr hMonitor, NativeMonitorInfo lpmi);
+
+
+        [Serializable, StructLayout(LayoutKind.Sequential)]
+        public struct NativeRectangle
+        {
+            public Int32 Left;
+            public Int32 Top;
+            public Int32 Right;
+            public Int32 Bottom;
+
+
+            public NativeRectangle(Int32 left, Int32 top, Int32 right, Int32 bottom)
+            {
+                this.Left = left;
+                this.Top = top;
+                this.Right = right;
+                this.Bottom = bottom;
+            }
+        }
+
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public sealed class NativeMonitorInfo
+        {
+            public Int32 Size = Marshal.SizeOf(typeof(NativeMonitorInfo));
+            public NativeRectangle Monitor;
+            public NativeRectangle Work;
+            public Int32 Flags;
+        }
+    }
+
+}
