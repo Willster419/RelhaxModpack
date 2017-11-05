884dc85beddc06c7a2659c06d8fc8772bd55ab4a
Willster419 <willster419@outlook.com>
2017-11-05 15:50:15 -0500

    Added RelhaxForum.cs. Used for all modpack forums, because it has the scaling code and title bar calculation code. No longer have to copy and paste the code for every form.

---
+++ RelicModManager/RelhaxForum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace RelhaxModpack
+{
+    class RelhaxForum : Form
+    {
+        private const int TitleBar = 23;//set origionaly for 23
+        private int TitleBarDifference = 0;
+        private int TitleHeight = 0;
+        protected override void OnLoad(EventArgs e)
+        {
+            //font scaling
+            AutoScaleMode = Settings.AppScalingMode;
+            Font = Settings.AppFont;
+            if (Settings.AppScalingMode == System.Windows.Forms.AutoScaleMode.Dpi)
+            {
+                this.Scale(new System.Drawing.SizeF(Settings.ScaleSize, Settings.ScaleSize));
+            }
+            //title bar calculation
+            Rectangle screenRektangle = RectangleToScreen(this.ClientRectangle);
+            TitleHeight = screenRektangle.Top - this.Top;
+            if (TitleHeight > TitleBar)
+            {
+                TitleBarDifference = TitleHeight - TitleBar;
+            }
+            base.OnLoad(e);
+        }
+    }
+}
