2e3a59c3588399d7b260fcc697b4378f0f9b41b8
Grumpelumpf <grumpelumpf@gmail.com>
2017-11-07 15:02:19 +0100

    moved AtlasesCreator code from AtlasesCreator section to InstallerComponents

---
+++ RelicModManager/InstallerComponents/Atlases.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RelhaxModpack
+{
+    public class Atlases
+    {
+        //a single string with the filename of the processingNativeFile (needed for tracing work instructions after installation)
+        public string nativeProcessingFile { get; set; } = "";
+        //the actual name of the original file before processed
+        public string actualPatchName { get; set; } = "";
+        // path and name to the package file
+        public string pkg { get; set; } = "";
+        // fileName of the atlas file to extract
+        public string atlasFile { get; set; } = "";
+        // fileName of the atlas map file to extract
+        public string mapFile { get; set; } = "";
+        // path inside the pkg file to the filename to process
+        public string directoryInArchive { get; set; } = "";
+        // path to place the finished file
+        public string atlasSaveDirectory { get; set; } = "";
+        // maybe new fileName
+        public List<string> imageFolderList { get; set; } = new List<string>();
+        // temp workingFolder
+        public string workingFolder { get; set; } = "";
+        //for the tostring thing
+        public override string ToString()
+        {
+            return string.Format("nativeProcessingFile: {0}\nactualPatchName: {1}\npkg: {2}\ndirectoryInArchive: {3}\natlasFile: {4}\natlasSaveDirectory: {5}\nimageFolderList: {6}",
+                nativeProcessingFile.Equals("") ? "(empty)" : nativeProcessingFile,
+                actualPatchName.Equals("") ? "(empty)" : actualPatchName,
+                pkg.Equals("") ? "(empty)" : pkg,
+                directoryInArchive.Equals("") ? "(empty)" : directoryInArchive,
+                atlasFile.Equals("") ? "(empty)" : atlasFile,
+                atlasSaveDirectory.Equals("") ? "(empty)" : atlasSaveDirectory,
+                imageFolderList.Count == 0 ? "(empty)" : imageFolderList.ToString());
+        }
+    }
+}
---
+++ RelicModManager/InstallerComponents/Texture.cs
@@ -0,0 +1,16 @@
+﻿namespace RelhaxModpack
+{
+    public class Texture
+    {
+        //name of the texture
+        public string name { get; set; }
+        //x position of the texture in the atlas image
+        public int x { get; set; }
+        //y position of the texture in the atlas image
+        public int y { get; set; }
+        //width of the texture in the atlas image
+        public int width { get; set; }
+        //height of the texture in the atlas image
+        public int height { get; set; }
+    }
+}
