23490dede5da949f42619ba9ae4be945013e731a
Willster419 <willster419@outlook.com>
2018-02-18 22:11:03 -0500

    Released beta version 27.8.0

--- RelicModManager/Properties/AssemblyInfo.cs
+++ RelicModManager/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.27.7.3")]
-[assembly: AssemblyFileVersion("0.27.7.3")]
+[assembly: AssemblyVersion("0.27.8.0")]
+[assembly: AssemblyFileVersion("0.27.8.0")]
 [assembly: NeutralResourcesLanguage("")]
