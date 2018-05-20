80845b23b63a442282339f287774d25346032a77
Grumpelumpf <grumpelumpf@gmail.com>
2018-05-20 21:39:18 +0200

    added dds test exporter (real fileformat currently PNG!)

--- RelicModManager/AtlasesCreator/Exporters/DdsImageExporter.cs
+++ RelicModManager/AtlasesCreator/Exporters/DdsImageExporter.cs
@@ -3,20 +3,8 @@ using System.Drawing.Imaging;
 
 namespace RelhaxModpack.AtlasesCreator
 {
-    /**
-    public class DdsImageExporter : IImageExporter
+    public class DssImageExporter : IImageExporter
     {
-        
-        internal int dwMagic { get; } = 0x20534444;
-        internal int dwsSize = 124;
-        internal int dwFlags = 0x0000000;
-        internal int dwHeight = 0;              // height of image
-        internal int dwWidth = 0;               // width of image
-        internal int dwLinearSize = 0;          // texture size
-        // internal byte[] Buffer1: array[0..59] of byte; // not important parameters
-        // internal TMagicChars dwFourCC: TMagicChars; // 'DXT1' or 'DXT5'
-        // internal byte[] Buffer2: array[0..39] of byte; // not important parameters
-
         public string ImageExtension
         {
             get { return "dds"; }
@@ -24,13 +12,7 @@ namespace RelhaxModpack.AtlasesCreator
 
         public void Save(string filename, Bitmap image)
         {
-            // image.Save(filename, ImageFormat.Bmp);
+            image.Save(filename, ImageFormat.Png);
         }
-
-        internal CreateHeader(Bitmap image)
-        {
-
-            dwMagic = 
-        } 
-    }**/
+    }
 }
