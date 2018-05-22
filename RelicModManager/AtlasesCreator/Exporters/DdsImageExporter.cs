25f337457bf54e6decd0f84b1e2d45f814a3abd4
Grumpelumpf <grumpelumpf@gmail.com>
2018-05-22 19:00:27 +0200

    added wokroing DDS image file export

--- RelicModManager/AtlasesCreator/Exporters/DdsImageExporter.cs
+++ RelicModManager/AtlasesCreator/Exporters/DdsImageExporter.cs
@@ -1,5 +1,8 @@
 ﻿using System.Drawing;
-using System.Drawing.Imaging;
+using System.IO;
+using System;
+using TeximpNet;
+using TeximpNet.DDS;
 
 namespace RelhaxModpack.AtlasesCreator
 {
@@ -12,7 +15,37 @@ namespace RelhaxModpack.AtlasesCreator
 
         public void Save(string filename, Bitmap image)
         {
-            image.Save(filename, ImageFormat.Png);
+            try
+            {
+                image.Save(Path.ChangeExtension(filename, ".png"), System.Drawing.Imaging.ImageFormat.Png);
+            }
+            catch (Exception ex)
+            {
+                Utils.ExceptionLog("DssImageExporter", "SavePNG", ex);
+                return;
+            }
+            Surface surfaceFromFile = null;
+            String file = null;
+            try
+            {
+                file = Path.ChangeExtension(filename, ".png");
+                surfaceFromFile = Surface.LoadFromFile(file);
+                surfaceFromFile.FlipVertically();
+            }
+            catch (Exception ex)
+            {
+                Utils.ExceptionLog("DssImageExporter", "LoadPNG", ex);
+            }
+            try
+            {
+                String fileOut = Path.ChangeExtension(filename, ".dds");
+                DDSFile.Write(fileOut, surfaceFromFile, TextureDimension.Two, DDSFlags.None);
+                File.Delete(file);
+            }
+            catch (Exception ex)
+            {
+                Utils.ExceptionLog("DssImageExporter", "SaveDDS", ex);
+            }
         }
     }
 }
