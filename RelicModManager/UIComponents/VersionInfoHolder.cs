87997a4246e8013af2705f95525cde1a331c1244
Grumpelumpf <grumpelumpf@gmail.com>
2018-07-03 20:46:17 +0200

    added class VersionInfoHolder

---
+++ RelicModManager/UIComponents/VersionInfoHolder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RelhaxModpack
+{
+    class VersionInfoHolder
+    {
+        public string LocalManagerVersion { get; set; } = "";
+        public string OnlineManagerVersion { get; set; } = "";
+        public string MinimumEditorVersion { get; set; } = "";
+        public string DatabaseVersion { get; set; } = ""; 
+    }
+}
