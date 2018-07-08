97aa7981f058b29c4bd9c41434522cb917f57269
Grumpelumpf <grumpelumpf@gmail.com>
2018-07-08 18:21:45 +0200

    added BackupFolderViewer and delete functions

--- RelicModManager/Forms/DeleteBackupFolder.Designer.cs
+++ RelicModManager/Forms/DeleteBackupFolder.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace RelhaxModpack.Forms
+﻿namespace RelhaxModpack
 {
     partial class DeleteBackupFolder
     {
@@ -30,25 +30,54 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DeleteBackupFolder));
             this.SelectBackupFolderPanel = new System.Windows.Forms.Panel();
+            this.CancelCloseButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // SelectBackupFolderPanel
             // 
+            this.SelectBackupFolderPanel.AutoScroll = true;
             this.SelectBackupFolderPanel.Location = new System.Drawing.Point(12, 25);
             this.SelectBackupFolderPanel.Name = "SelectBackupFolderPanel";
-            this.SelectBackupFolderPanel.Size = new System.Drawing.Size(266, 201);
+            this.SelectBackupFolderPanel.Size = new System.Drawing.Size(301, 201);
             this.SelectBackupFolderPanel.TabIndex = 0;
+            // 
+            // CancelCloseButton
+            // 
+            this.CancelCloseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.CancelCloseButton.Location = new System.Drawing.Point(12, 232);
+            this.CancelCloseButton.Name = "CancelCloseButton";
+            this.CancelCloseButton.Size = new System.Drawing.Size(75, 23);
+            this.CancelCloseButton.TabIndex = 3;
+            this.CancelCloseButton.Text = "Cancel";
+            this.CancelCloseButton.UseVisualStyleBackColor = true;
+            this.CancelCloseButton.Click += new System.EventHandler(this.CancelCloseButton_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.DeleteButton.Location = new System.Drawing.Point(238, 232);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(75, 23);
+            this.DeleteButton.TabIndex = 4;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
             // 
             // DeleteBackupFolder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(290, 267);
+            this.ClientSize = new System.Drawing.Size(325, 267);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.CancelCloseButton);
             this.Controls.Add(this.SelectBackupFolderPanel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(300, 300);
             this.Name = "DeleteBackupFolder";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.DeleteBackupFolder_FormClosing);
             this.ResumeLayout(false);
 
         }
@@ -56,5 +85,7 @@
         #endregion
 
         private System.Windows.Forms.Panel SelectBackupFolderPanel;
+        private System.Windows.Forms.Button CancelCloseButton;
+        private System.Windows.Forms.Button DeleteButton;
     }
 }
--- RelicModManager/Forms/DeleteBackupFolder.cs
+++ RelicModManager/Forms/DeleteBackupFolder.cs
@@ -1,19 +1,92 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Windows.Forms;
 
-namespace RelhaxModpack.Forms
+namespace RelhaxModpack
 {
-    public partial class DeleteBackupFolder : Form
+    public partial class DeleteBackupFolder : RelhaxForum
     {
-        public DeleteBackupFolder()
+        // this is needed to "communicate" from SubForm to MainForm
+        public MainWindow MyParent { get; set; }
+
+        public DeleteBackupFolder(List<BackupFolder> bfl)
         {
             InitializeComponent();
+            CancelCloseButton.Text = Translations.GetTranslatedString("cancel");
+            DeleteButton.Text = Translations.GetTranslatedString("delete");
+            DeleteBackupFolder_Load(bfl);
+        }
+
+        private void DeleteBackupFolder_Load(List<BackupFolder> bfl)
+        {
+            foreach (BackupFolder bf in bfl)
+            {
+                if (!Directory.Exists(bf.TopfolderName)) continue;
+                Utils.ConvertDateToLocalCultureFormat(Path.GetFileName(bf.TopfolderName), out string cultureDate);
+                SelectionCheckBox cb = new SelectionCheckBox
+                {
+                    Directory = bf.TopfolderName,
+                    NameList = bf.FullnameList,
+                    Text = string.Format("{0} ({1})", cultureDate, Utils.SizeSuffix(bf.FilesSizeOnDisk, 2, true)),
+                    Name = bf.TopfolderName
+                };
+                cb.AutoSize = true;
+                cb.Location = new Point(6, (SelectBackupFolderPanel.Controls.Count * (cb.Size.Height - 3)));
+                // add ToolTip to the SelectBackupFolder
+                ToolTip rbToolTip = new ToolTip
+                {
+                    // Set up the delays for the ToolTip.
+                    AutoPopDelay = 30000,
+                    InitialDelay = 1000,
+                    ReshowDelay = 500,
+                    // Force the ToolTip text to be displayed whether or not the form is active.
+                    ShowAlways = true
+                };
+                // Set up the ToolTip text for the Button and Checkbox.
+                rbToolTip.SetToolTip(cb, string.Format("foldername: {4}\nfiles: {0}\nfolders: {1}\nsize: {2}\nsize on disk: {3}\nlast modified: {5}", bf.FileCount, bf.FolderCount, Utils.SizeSuffix(bf.FilesSize, 2, true), Utils.SizeSuffix(bf.FilesSizeOnDisk, 2, true), bf.TopfolderName, Directory.GetLastWriteTime(bf.TopfolderName).ToString()));
+                SelectBackupFolderPanel.Controls.Add(cb);
+            }
+        }
+
+        private void CancelCloseButton_Click(object sender, System.EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void DeleteButton_Click(object sender, System.EventArgs e)
+        {
+            if (MessageBox.Show("Realy delete all this backup folders?\n\nIt will clean up XX file and give you XX GB additional free HDD space.", "WARNING", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2) != DialogResult.Yes)
+                return;
+            List<Control> controlToRemove = new List<Control>();
+            foreach (Control bf in this.SelectBackupFolderPanel.Controls)
+            {
+                if (bf is SelectionCheckBox scb)
+                {
+                    if (scb.Checked)
+                    {
+                        Logging.Manager("deleting backup: " + scb.Directory);
+                        Utils.FileDelete(scb.NameList);
+                        controlToRemove.Add(scb);
+                    }
+                }
+            }
+            foreach (Control rc in controlToRemove)
+            {
+                rc.Dispose();
+            }
+            int count = 0;
+            foreach (Control bf in this.SelectBackupFolderPanel.Controls)
+            {
+                bf.Location = new Point(6, (count * bf.Size.Height));
+                count++;
+            }
+        }
+
+        private void DeleteBackupFolder_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.MyParent.ScanningRelHaxModBackupFolder();
+            this.Dispose();
         }
     }
 }
