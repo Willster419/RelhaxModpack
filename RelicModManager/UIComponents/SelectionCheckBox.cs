f96f067f16868e07712066c7062eb0227596c2c9
Grumpelumpf <grumpelumpf@gmail.com>
2018-07-08 17:56:16 +0200

    added SelectionCheckBox for BackupViewer

---
+++ RelicModManager/UIComponents/SelectionCheckBox.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace RelhaxModpack
+{
+    class SelectionCheckBox : CheckBox
+    {
+       public string Directory { get; set; }
+       public List<string> NameList { get; set; }
+    }
+}
