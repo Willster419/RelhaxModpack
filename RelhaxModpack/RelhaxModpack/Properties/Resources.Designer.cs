4f0669503e0b285e26e0638d66e4af4188f98620
Willster419 <willster419@outlook.com>
2018-08-11 18:19:33 -0400

    Added images and dll files to resources

--- RelhaxModpack/RelhaxModpack/Properties/Resources.Designer.cs
+++ RelhaxModpack/RelhaxModpack/Properties/Resources.Designer.cs
@@ -61,12 +61,285 @@ namespace RelhaxModpack.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to                   GNU LESSER GENERAL PUBLIC LICENSE
+        ///                       Version 2.1, February 1999
+        ///
+        /// Copyright (C) 1991, 1999 Free Software Foundation, Inc.
+        /// 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+        /// Everyone is permitted to copy and distribute verbatim copies
+        /// of this license document, but changing it is not allowed.
+        ///
+        ///(This is the first released version of the Lesser GPL.  It also counts
+        /// as the successor of the GNU Library Public License, version 2, hence
+        /// the version numb [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string dds_reader {
+            get {
+                return ResourceManager.GetString("dds_reader", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap discord_brand {
+            get {
+                object obj = ResourceManager.GetObject("discord_brand", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap donation {
+            get {
+                object obj = ResourceManager.GetObject("donation", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] DotNetZip {
+            get {
+                object obj = ResourceManager.GetObject("DotNetZip", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap EMail {
+            get {
+                object obj = ResourceManager.GetObject("EMail", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap facebook_brand {
+            get {
+                object obj = ResourceManager.GetObject("facebook_brand", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap flag_de {
+            get {
+                object obj = ResourceManager.GetObject("flag_de", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap flag_gb {
+            get {
+                object obj = ResourceManager.GetObject("flag_gb", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap flag_us {
+            get {
+                object obj = ResourceManager.GetObject("flag_us", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Home {
+            get {
+                object obj = ResourceManager.GetObject("Home", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap loading {
+            get {
+                object obj = ResourceManager.GetObject("loading", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap loading_3rdguards {
+            get {
+                object obj = ResourceManager.GetObject("loading_3rdguards", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
         internal static System.Drawing.Icon modpack_icon {
             get {
                 object obj = ResourceManager.GetObject("modpack_icon", resourceCulture);
                 return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to                                  Apache License
+        ///                           Version 2.0, January 2004
+        ///                        http://www.apache.org/licenses/
+        ///
+        ///   TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
+        ///
+        ///   1. Definitions.
+        ///
+        ///      &quot;License&quot; shall mean the terms and conditions for use, reproduction,
+        ///      and distribution as defined by Sections 1 through 9 of this document.
+        ///
+        ///      &quot;Licensor&quot; shall mean the copyright owner or entity authorized by
+        ///      the copyright owner that is [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string modpack_license_agrement {
+            get {
+                return ResourceManager.GetString("modpack_license_agrement", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] NAudio {
+            get {
+                object obj = ResourceManager.GetObject("NAudio", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Microsoft Public License (Ms-PL)
+        ///
+        ///This license governs use of the accompanying software. If you use the software, you accept this license. If you do not accept the license, do not use the software.
+        ///
+        ///1. Definitions
+        ///
+        ///The terms &quot;reproduce,&quot; &quot;reproduction,&quot; &quot;derivative works,&quot; and &quot;distribution&quot; have the same meaning here as under U.S. copyright law.
+        ///
+        ///A &quot;contribution&quot; is the original software, or any additions or changes to the software.
+        ///
+        ///A &quot;contributor&quot; is any person that distributes its contribution [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string NAudio1 {
+            get {
+                return ResourceManager.GetString("NAudio1", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] Newtonsoft_Json {
+            get {
+                object obj = ResourceManager.GetObject("Newtonsoft_Json", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap play_pause {
+            get {
+                object obj = ResourceManager.GetObject("play_pause", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap report {
+            get {
+                object obj = ResourceManager.GetObject("report", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Copyright (c) 2009-2010 Nick Gravelyn (nick@gravelyn.com), Markus Ewald (cygon@nuclex.org)
+        ///
+        ///Permission is hereby granted, free of charge, to any person obtaining a 
+        ///copy of this software and associated documentation files (the &quot;Software&quot;), 
+        ///to deal in the Software without restriction, including without limitation 
+        ///the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+        ///and/or sell copies of the Software, and to permit persons to whom the Software 
+        ///is furnished to do so, subject to t [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string SpriteSheetPacker {
+            get {
+                return ResourceManager.GetString("SpriteSheetPacker", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap stop2 {
+            get {
+                object obj = ResourceManager.GetObject("stop2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Copyright (c) 2016-2018 TeximpNet - Nicholas Woodfield
+        /// 
+        ///Permission is hereby granted, free of charge, to any person obtaining a copy
+        ///of this software and associated documentation files (the &quot;Software&quot;), to deal
+        ///in the Software without restriction, including without limitation the rights
+        ///to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        ///copies of the Software, and to permit persons to whom the Software is
+        ///furnished to do so, subject to the following conditions:
+        ///
+        ///The above co [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string TeximpNet {
+            get {
+                return ResourceManager.GetString("TeximpNet", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap twitter_brand {
+            get {
+                object obj = ResourceManager.GetObject("twitter_brand", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap WoT_brand_light_grey {
+            get {
+                object obj = ResourceManager.GetObject("WoT_brand_light_grey", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
