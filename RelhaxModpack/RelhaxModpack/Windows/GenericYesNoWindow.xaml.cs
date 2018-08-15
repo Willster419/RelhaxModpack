d4d42b95b3d2a220d7f663f7b71dffa5e522ef24
Willster419 <willster419@outlook.com>
2018-08-15 16:03:00 -0400

    Added icons to all currents windows. Added GenericYesNoWindow, renamed selectionViewer to GenericWindow

---
+++ RelhaxModpack/RelhaxModpack/Windows/GenericYesNoWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RelhaxModpack.Windows
+{
+    /// <summary>
+    /// Interaction logic for GenericYesNoWindow.xaml
+    /// </summary>
+    public partial class GenericYesNoWindow : Window
+    {
+        public GenericYesNoWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Apply Translations
+
+        }
+
+        private void OnNoButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnYesButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
