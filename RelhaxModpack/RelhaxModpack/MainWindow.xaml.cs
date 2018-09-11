6609f36bc843bcde04101a7eaed9ff0cc1737d26
Willster419 <willster419@outlook.com>
2018-09-11 01:54:18 -0400

    Added most event handlers for when settings changed in MainWindow

--- RelhaxModpack/RelhaxModpack/MainWindow.xaml.cs
+++ RelhaxModpack/RelhaxModpack/MainWindow.xaml.cs
@@ -79,5 +79,115 @@ namespace RelhaxModpack
             //create menu options
 
         }
+
+        private void OnSelectionViewChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EnableBordersCB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnMulticoreExtractionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnCreateShortcutsChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnSaveUserDataChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnClearWoTCacheChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnClearLogFilesChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnCleanInstallChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnImmidateExtarctionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShowInstallCompleteWindowCB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnShowInstallCompleteWindowChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnBackupModsChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnPreviewLoadingImageChange(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnForceManuelGameDetectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnInformIfNoNewDatabaseChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnSaveLastInstallChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnUseBetaAppChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnUseBetaDatabaseChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnDefaultBordersChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnDefaultSelectColorChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnLegacyBordersChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnLegacySelectColorChenged(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
