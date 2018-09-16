8d924086466922b8f6af3878bf551921d91d6dde
Willster419 <willster419@outlook.com>
2018-09-16 12:56:51 -0700

    Added code-behind for ProgressIndicator

--- RelhaxModpack/RelhaxModpack/Windows/ProgressIndicator.xaml.cs
+++ RelhaxModpack/RelhaxModpack/Windows/ProgressIndicator.xaml.cs
@@ -19,9 +19,67 @@ namespace RelhaxModpack.Windows
     /// </summary>
     public partial class ProgressIndicator : Window
     {
+        public double ProgressMinimum
+        {
+            get { return LoadingProgressBar.Minimum; }
+            set { LoadingProgressBar.Minimum = value; }
+        }
+
+        public double ProgressMaximum
+        {
+            get { return LoadingProgressBar.Maximum; }
+            set { LoadingProgressBar.Maximum = value; }
+        }
+        
+        public string Message
+        {
+            get { return LoadingText.Text; }
+            set { LoadingText.Text = value; }
+        }
+
         public ProgressIndicator()
         {
             InitializeComponent();
+        }
+
+        private void OnWindowLoad(object sender, RoutedEventArgs e)
+        {
+            //TODO: happends after propeties set?
+            if (ProgressMinimum != 0)
+                Logging.WriteToLog("progress minimum value is not 0! (is this the intent?)", Logfiles.Application, LogLevel.Warning);
+            if (ProgressMaximum <= 0)
+            {
+                Logging.WriteToLog("progress maximum is less than or equal to 0! (is this the intent?) setting to default of 100",
+                    Logfiles.Application, LogLevel.Warning);
+            }
+            //apply translations
+            Translations.LocalizeWindow(this, false);
+        }
+
+        public void UpdateProgress(double percent)
+        {
+            if(percent > LoadingProgressBar.Maximum)
+            {
+                Logging.WriteToLog(string.Format("percent value of {0} is greator than maximu set value of {1}, setting to maxumum instead"
+                    , percent, LoadingProgressBar.Maximum), Logfiles.Application, LogLevel.Warning);
+                LoadingProgressBar.Value = LoadingProgressBar.Maximum;
+            }
+            else if (percent < 0)
+            {
+                Logging.WriteToLog(string.Format("percent value of {0} is less than 0, setting to maxumum instead", percent, LoadingProgressBar.Minimum),
+                    Logfiles.Application, LogLevel.Warning);
+                LoadingProgressBar.Value = LoadingProgressBar.Minimum;
+            }
+            else
+            {
+                LoadingProgressBar.Value = percent;
+            }
+        }
+
+        public void UpdateProgress(double percent, string updatedmessage)
+        {
+            UpdateProgress(percent);
+            Message = updatedmessage;
         }
     }
 }
