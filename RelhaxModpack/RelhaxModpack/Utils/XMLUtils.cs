9d97985c6dc1d16e09829be9bdbc88f0ade425ea
Willster419 <willster419@outlook.com>
2018-10-07 22:32:06 -0700

    started work on entire re-write of xml parsing of database, for "2.0" version of database

--- RelhaxModpack/RelhaxModpack/Utils/XMLUtils.cs
+++ RelhaxModpack/RelhaxModpack/Utils/XMLUtils.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using System.Reflection;
 
 namespace RelhaxModpack
 {
@@ -15,6 +18,24 @@ namespace RelhaxModpack
     {
         //static path for all developer selections
         public const string DeveloperSelectionsXPath = "TODO";
+        #region Components of packages
+        //for base package
+        public static readonly string[] BaseRequiredAttributes = new string[] { "PackageName", "Enabled" };
+        public static readonly string[] BaseOptionalAttributes = new string[] { "Version", "Timestamp", "ZipFile", "CRC", "StartAddress",
+            "EndAddress", "AppendExtraction", "LogAtInstall", "DevURL", "ExtractionLevel" };
+        //for dependency
+        public static readonly string[] DependnecyRequiredAttributes = new string[] { "LogicType" };
+        //for selectable package
+        public static readonly string[] SelectablePackageRequiredAttributes = new string[] { "Name", "Type" };
+        public static readonly string[] SelectablePackageOptionalAttributes = new string[] { "Visible", "Size", "UpdateComment", "Description" };
+        public static readonly string[] SelectablePackagOptionaleNodes = new string[] { "UserFiles", "Packages", "PictureList" };
+        //for both:
+        public static readonly string[] DependencySelectableOptionalNodes = new string[] { "Dependencies" };
+        //inside packages - component required attributes
+        public static readonly string[] DependnecyLogicRequiredAttributes = new string[] { "NegateFlag", "PackageName" };
+        public static readonly string[] MediaRequiredAttributes = new string[] { "URL", "MediaType" };
+        public static readonly string[] UserfilesRequiredAttributes = new string[] { "Pattern" };
+        #endregion
         //check to make sure an xml file is valid
         public static bool IsValidXml(string filePath)
         {
@@ -161,13 +182,155 @@ namespace RelhaxModpack
           }
           return results;
         }
-        public static void ParseDatabase()
+        public static bool ParseDatabase(XmlDocument modInfoDocument, List<Dependency> globalDependencies,
+            List<Dependency> logicalDependencies, List<Category> parsedCategoryList)
         {
-            //stub
+            Logging.WriteToLog("start of ParseDatabase()", Logfiles.Application, LogLevel.Debug);
+            //check all input parameters
+            if (modInfoDocument == null)
+                throw new BadMemeException("modInfoDocument is null dumbass");
+            if (globalDependencies == null)
+                globalDependencies = new List<Dependency>();
+            else
+                globalDependencies.Clear();
+            if (logicalDependencies == null)
+                logicalDependencies = new List<Dependency>();
+            else
+                logicalDependencies.Clear();
+            if (parsedCategoryList == null)
+                parsedCategoryList = new List<Category>();
+            else
+                parsedCategoryList.Clear();
+            //determine which version of the document we are loading. allows for loading of different versions if structure change
+            string versionString = GetXMLStringFromXPath(modInfoDocument, "//modInfoAlpha.xml@documentVersion");
+            Logging.WriteToLog(nameof(versionString) + "=" + versionString, Logfiles.Application, LogLevel.Debug);
+            switch(versionString)
+            {
+                case "2.0":
+                    return ParseDatabaseV2(DocumentToXDocument(modInfoDocument), globalDependencies, logicalDependencies, parsedCategoryList);
+                default:
+                    Logging.WriteToLog(string.Format("unknown format of {0}: {1}", nameof(versionString), versionString),
+                        Logfiles.Application, LogLevel.Error);
+                    return false;
+            }
+        }
+        public static bool ParseDatabaseV2(XDocument modInfoDocument, List<Dependency> globalDependencies,
+            List<Dependency> logicalDependencies, List<Category> parsedCategoryList)
+        {
+            //parsing the global dependencies
+            bool globalParsed = ParseDatabaseV2GlobalDependencies(
+                modInfoDocument.XPathSelectElements("/modInfoAlpha.xml/globaldependencies/globaldependency").ToList(), globalDependencies);
+            //parsing the logical dependnecies
+            bool logicalDepParsed = ParseDatabaseV2LogicalDependencies(modInfoDocument.XPathSelectElements(
+                "/modInfoAlpha.xml/logicalDependencies/logicalDependency").ToList(), logicalDependencies);
+            //parsing the categories
+            bool categoriesParsed = ParseDatabaseV2Categories(modInfoDocument.XPathSelectElements(
+                "/modInfoAlpha.xml/catagories/catagory").ToList(), parsedCategoryList);
+            return (globalParsed && logicalDepParsed && categoriesParsed) ? true : false;
+        }
+        private static bool ParseDatabaseV2GlobalDependencies(List<XElement> holder, List<Dependency> globalDependencies)
+        {
+            //first for loop is for each "dependency" object holder
+            foreach(XElement dependency in holder)
+            {
+                Dependency globalDependency = new Dependency();
+                
+            }
+            return true;
+        }
+        private static void DynamicXMLParse(XElement dependency, ref DatabasePackage package)
+        {
+            //first treat it as regular package
+            //check for all fields in it
+            List<string> copyOfRequiredBaseAttributes = new List<string>(BaseRequiredAttributes.ToList());
+            List<string> unknownElements = new List<string>();
+            foreach(XAttribute attribute in dependency.Attributes())//ATTRIBUTES
+            {
+                string attributeName = attribute.Name.LocalName;
+                if (copyOfRequiredBaseAttributes.Contains(attributeName))
+                {
+                    //switch
+                    copyOfRequiredBaseAttributes.Remove(attributeName);
+                }
+                else if (BaseOptionalAttributes.Contains(attributeName))
+                {
+                    //switch
+
+                }
+                else
+                    unknownElements.Add(attributeName);
+            }
+            //check if it's logical or selectable
+            if(package is Dependency dep)
+            {
+                DynamicXMLParse(dependency, ref dep, ref unknownElements);
+            }
+            else if (package is SelectablePackage sp)
+            {
+                DynamicXMLParse(dependency, ref sp, ref unknownElements);
+            }
+            else
+            {
+                Logging.WriteToLog("Package type should not get here", Logfiles.Application, LogLevel.Error);
+            }
+        }
+        private static void DynamicXMLParse(XElement dependency, ref Dependency package, ref List<string> unknownElements)
+        {
+            //copy list of required
+
+            //process list of attributes
+
+            //process list of nodes
+            /*foreach(XElement element in dependency.Elements())//ELEMENTS
+            {
+                string elementName = element.Name.LocalName;
+                if (OptionalBaseNodes.Contains(elementName))
+                {
+                    //switch
+                }
+                else
+                    unknownElements.Add(elementName);
+            }*/
+        }
+        private static void DynamicXMLParse(XElement dependency, ref SelectablePackage package, ref List<string> unknownElements)
+        {
+            //copy list of required
+
+            //process list of attributes
+
+            //process list of nodes
+            /*foreach(XElement element in dependency.Elements())//ELEMENTS
+            {
+                string elementName = element.Name.LocalName;
+                if (OptionalBaseNodes.Contains(elementName))
+                {
+                    //switch
+                }
+                else
+                    unknownElements.Add(elementName);
+            }*/
+        }
+        private static bool ParseDatabaseV2LogicalDependencies(List<XElement> holder, List<Dependency> logicalDependencies)
+        {
+
+            return true;
+        }
+        private static bool ParseDatabaseV2Categories(List<XElement> holder, List<Category> categories)
+        {
+
+            return true;
         }
         public static void CheckCRC()
         {
             //stub
         }
+        //https://blogs.msdn.microsoft.com/xmlteam/2009/03/31/converting-from-xmldocument-to-xdocument/
+        private static XDocument DocumentToXDocument(XmlDocument doc)
+        {
+            if (doc == null)
+                return null;
+            return XDocument.Parse(doc.OuterXml,LoadOptions.SetLineInfo);
+        }
     }
 }
+ 
