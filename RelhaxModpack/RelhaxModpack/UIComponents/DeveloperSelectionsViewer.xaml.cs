12669047ee23e5adf753c927a3c4272a718fe6d6
Willster419 <willster419@outlook.com>
2018-11-15 20:22:27 -0700

    added a lot of the code-behind for DeveloperSelectionsViewer but i can't test it until

--- RelhaxModpack/RelhaxModpack/UIComponents/DeveloperSelectionsViewer.xaml.cs
+++ RelhaxModpack/RelhaxModpack/UIComponents/DeveloperSelectionsViewer.xaml.cs
@@ -10,7 +10,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Net;
+using System.IO;
+using System.Xml;
 
 namespace RelhaxModpack.Windows
 {
@@ -25,9 +27,14 @@ namespace RelhaxModpack.Windows
     /// </summary>
     public partial class DeveloperSelectionsViewer : RelhaxWindow
     {
+        //init it to false so that it only will get changed to true at that one point when it works
         private bool loadSelection = false;
         private string fileToLoad = "";
         public event DeveloperSelectionsClosedDelagate OnDeveloperSelectionsClosed;
+        private WebClient client;
+        private bool currentlyDownloading = false;
+        private string databaseURL = "http://wotmods.relhaxmodpack.com/RelhaxModpack/Rescoruces/developerSelections/developerSelections.zip";
+        private string databaseSaveLocation = Path.Combine(Settings.RelhaxTempFolder, "developerSelections.zip");
 
         public DeveloperSelectionsViewer()
         {
@@ -37,7 +44,53 @@ namespace RelhaxModpack.Windows
         private void OnApplicationLoading(object sender, RoutedEventArgs e)
         {
             //add loading message to stack panel
-            //event based wait (for easy canceling?
+            string forHeader = Translations.GetTranslatedString("loadingDevTranslations");
+            //add it to the thing here
+            currentlyDownloading = true;
+            using (client = new WebClient())
+            {
+                client.DownloadFileCompleted += OnSelectionsDownloaded;
+                try
+                {
+                    if (File.Exists(databaseSaveLocation))
+                        File.Delete(databaseSaveLocation);
+                    client.DownloadFileAsync(new Uri(databaseURL), databaseSaveLocation);
+                }
+                catch(Exception ex)
+                {
+                    Logging.WriteToLog(ex.ToString(), Logfiles.Application, LogLevel.Exception);
+                    MessageBox.Show(Translations.GetTranslatedString("failedToDownload") + "DeveloperSelections.zip");
+                    this.Close();
+                }
+            }
+        }
+
+        private void OnSelectionsDownloaded(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            if(e.Error != null)
+            {
+                //quit now
+                //TODO
+                return;
+            }
+            string selectionsXMlString = Utils.GetStringFromZip(databaseSaveLocation,"selections.xml");
+            if(string.IsNullOrWhiteSpace(selectionsXMlString))
+            {
+                Logging.WriteToLog("Failed to parse selections.xml from developerSelections.zip", Logfiles.Application, LogLevel.Error);
+                MessageBox.Show(Translations.GetTranslatedString("failedToParse") + "DeveloperSelections.zip");
+                return;
+            }
+            XmlDocument doc = new XmlDocument();
+            try
+            {
+                doc.LoadXml(selectionsXMlString);
+            }
+            catch (XmlException xmlex)
+            {
+                Logging.WriteToLog(xmlex.ToString(), Logfiles.Application, LogLevel.Exception);
+                MessageBox.Show(Translations.GetTranslatedString("failedToDownload") + "DeveloperSelections.zip");
+                return;
+            }
         }
 
         private void OnApplicationClosed(object sender, EventArgs e)
