1a5317f552cf45b3b1b64e41214f02175f254504
Willster419 <willster419@outlook.com>
2019-03-10 17:39:52 -0700

    successfully able to set checkbox custom checkbox disabled property

--- RelhaxWPFConvert/RelhaxWPFConvert/RelhaxCheckBox.xaml.cs
+++ RelhaxWPFConvert/RelhaxWPFConvert/RelhaxCheckBox.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,13 +19,34 @@ namespace RelhaxWPFConvert
     /// <summary>
     /// Interaction logic for RelhaxCheckBox.xaml
     /// </summary>
-    public partial class RelhaxCheckBox : CheckBox
+    public partial class RelhaxCheckBox : CheckBox, INotifyPropertyChanged
     {
         public SolidColorBrush CheckboxDisabledColorBrush { get; set; } = new SolidColorBrush(Colors.Red);
+        private Color _CheckboxDisabledColor = Colors.Red;
+        public Color CheckboxDisabledColor
+        {
+            get
+            {
+                return _CheckboxDisabledColor;
+            }
+            set
+            {
+                _CheckboxDisabledColor = value;
+                OnPropertyChanged(nameof(CheckboxDisabledColor));
+            }
+        }
 
         public RelhaxCheckBox()
         {
             InitializeComponent();
+        }
+        //https://stackoverflow.com/questions/34651123/wpf-binding-a-background-color-initializes-but-not-updating
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            var handle = PropertyChanged;
+            if (handle != null)
+                handle(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
