46005753f742fdff855f82125ccde74f4eb161d3
Willster419 <willster419@outlook.com>
2019-04-28 12:50:49 -0700

    reverted UserFile changes

--- RelhaxModpack/RelhaxModpack/DatabaseComponents/UserFile.cs
+++ RelhaxModpack/RelhaxModpack/DatabaseComponents/UserFile.cs
@@ -10,13 +10,21 @@ namespace RelhaxModpack
         // this could be a single file or a search pattern with * or ?
         public string Pattern = "";
 
+        //TEMP MAKE stuff below that's to remove properties so it's not picked up by fieldInfo
+
+        // will try to speed up the restore backup function in case of ClanIcons, the "backup folder" will be pushed back at once (and not file by file)
+        public bool placeBeforeExtraction { get; set; } = false;//TODO: remove?
+
+        // this entry will be pßroceeded in any case (if package is checked), even if "save user data" option is "false"
+        public bool systemInitiated { get; set; } = false;//TODO: remove
+
         //make this a property to be ignored also by xml saving
         //contain the list of actuall files saved. it's the full path inlcuding the file name
         public List<string> Files_saved { get; set; } = new List<string>();
 
         public override string ToString()
         {
-            return Pattern;
+            return Pattern + (placeBeforeExtraction ?  " (pre)" : "") + (systemInitiated ? " (sys)" : "");
         }
     }
 }
