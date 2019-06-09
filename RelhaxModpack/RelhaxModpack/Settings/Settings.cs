3399501da8b7d96cb9c4021d8a99115e683eb9cf
Willster419 <willster419@outlook.com>
2019-06-09 16:18:13 -0700

    renamed RelicCopyUpdate to relic_self_updater for Relhax V2 (also means we can start making changes to the V2 script if need be)

--- RelhaxModpack/RelhaxModpack/Settings/Settings.cs
+++ RelhaxModpack/RelhaxModpack/Settings/Settings.cs
@@ -58,8 +58,8 @@ namespace RelhaxModpack
         public const string ApplicationUpdateURL = "http://bigmods.relhaxmodpack.com/RelhaxModpack/RelhaxModpack.zip";
         public const string ApplicationBetaUpdateURL = "http://bigmods.relhaxmodpack.com/RelhaxModpack/RelhaxModpackBeta.zip";
         public const string ApplicationUpdateFileName = "RelhaxModpack_update.zip";
-        public const string RelicBatchUpdateScript = "RelicCopyUpdate.bat"; //"relic_self_updater.bat";
-        public const string RelicBatchUpdateScriptServer = "RelicCopyUpdate.txt"; //"relic_self_updater.txt";
+        public const string RelicBatchUpdateScript = "relic_self_updater.bat";
+        public const string RelicBatchUpdateScriptServer = "relic_self_updater.txt";
 
         public const string SelectionsRoot = "https://raw.githubusercontent.com/Willster419/RelhaxModpackDatabase/master/selection_files/";
         public const string SelectionsXml = "selections.xml";
