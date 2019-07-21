f76f80578caa01470a6c608efab56e3e30e0c606
Willster419 <willster419@outlook.com>
2019-07-20 17:31:36 -0700

    Documentation for Trigger and UserFile

--- RelhaxModpack/RelhaxModpack/DatabaseComponents/Trigger.cs
+++ RelhaxModpack/RelhaxModpack/DatabaseComponents/Trigger.cs
@@ -12,15 +12,29 @@ namespace RelhaxModpack
     /// </summary>
     public class Trigger
     {
-
+        /// <summary>
+        /// The name of the trigger
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// The total number of instances that this trigger exists in the selected packages to install
+        /// </summary>
         public int Total { get; set; }
 
+        /// <summary>
+        /// The number of processed triggers for this trigger type. Prevents the trigger from firing early
+        /// </summary>
         public int NumberProcessed { get; set; }
 
+        /// <summary>
+        /// Flag to determine if the trigger task has started
+        /// </summary>
         public bool Fired { get; set; }
 
+        /// <summary>
+        /// The reference for the task that the trigger should perform
+        /// </summary>
         public Task TriggerTask { get; set; }
     }
 }
