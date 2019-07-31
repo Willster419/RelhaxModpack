5017e032c9edeb4e5e3df64127804c6a00a2954a
Willster419 <willster419@outlook.com>
2019-07-30 21:25:45 -0700

    Documented Logging

--- RelhaxModpack/RelhaxModpack/Logging/Logging.cs
+++ RelhaxModpack/RelhaxModpack/Logging/Logging.cs
@@ -10,22 +10,33 @@ namespace RelhaxModpack
     public enum Logfiles
     {
         /// <summary>
-        /// The default modpack logfile
+        /// The default modpack log file
         /// </summary>
         Application,
+
         /// <summary>
-        /// logfile for when installing mods
+        /// The log file for when installing mods
         /// </summary>
         Installer,
+
         /// <summary>
-        /// logfile for when uninstalling mods
+        /// The log file for when uninstalling mods
         /// </summary>
         Uninstaller,
 
+        /// <summary>
+        /// The log file for the editor
+        /// </summary>
         Editor,
 
+        /// <summary>
+        /// The log file for the patcher
+        /// </summary>
         Patcher,
 
+        /// <summary>
+        /// The log file for the database update tool
+        /// </summary>
         Updater
     }
     /// <summary>
@@ -59,10 +70,14 @@ namespace RelhaxModpack
         ApplicationHalt
     }
 
+    /// <summary>
+    /// Delegate for allowing method callback with the written formatted message as the return value
+    /// </summary>
+    /// <param name="message">The formatted message that was written to the logfile</param>
     public delegate void LoggingUIThreadReport(string message);
 
     /// <summary>
-    /// A static constant refrence to common logging variables and common log refrences
+    /// A static constant reference to common logging variables and common log references
     /// </summary>
     public static class Logging
     {
@@ -70,48 +85,90 @@ namespace RelhaxModpack
         /// The filename of the application log file
         /// </summary>
         public const string ApplicationLogFilename = "Relhax.log";
+
+        /// <summary>
+        /// The filename of the updater log file
+        /// </summary>
         public const string ApplicationUpdaterLogFilename = "RelhaxUpdater.log";
+
+        /// <summary>
+        /// The filename of the editor log file
+        /// </summary>
         public const string ApplicationEditorLogFilename = "RelhaxEditor.log";
+
+        /// <summary>
+        /// The filename of the patch designer
+        /// </summary>
         public const string ApplicationPatchDesignerLogFilename = "RelhaxPatchDesigner.log";
+
         /// <summary>
         /// The filename of the old application log file
         /// </summary>
         public const string OldApplicationLogFilename = "RelHaxLog.txt";
+
         /// <summary>
         /// The name of the install log file
         /// </summary>
         public const string InstallLogFilename = "installedRelhaxFiles.log";
+
+        /// <summary>
+        /// The name of the backup install log file. During an install process, it backups the current one to provide a history of 1.
+        /// </summary>
         public const string InstallLogFilenameBackup = "installedRelhaxFiles.bak";
+
         /// <summary>
         /// the name of the uninstall log file
         /// </summary>
         public const string UninstallLogFilename = "uninstallRelhaxFiles.log";
+
+        /// <summary>
+        /// The name of the backup uninstall log file. During an uninstall process, it backups the current one to provide a history of 1.
+        /// </summary>
         public const string UninstallLogFilenameBackup = "uninstallRelhaxFiles.bak";
+
+        /// <summary>
+        /// The string time format for log entries
+        /// </summary>
         public const string ApplicationLogfileTimestamp = "yyyy-MM-dd HH:mm:ss.fff";
+        
         /// <summary>
         /// The header and end that shows the start and stop of the application log file
         /// </summary>
         public const string ApplicationlogStartStop = "|------------------------------------------------------------------------------------------------|";
+
         /// <summary>
-        /// Provides a constant refrence to the log file
+        /// Provides a constant reference to the log file
         /// </summary>
         private static Logfile ApplicationLogfile;
+
         /// <summary>
-        /// Provides a refrence to an instance of an install log file
+        /// Provides a reference to an instance of an install log file
         /// </summary>
         private static Logfile InstallLogfile;
+
         /// <summary>
-        /// Provides a refrence to an instalce of an uninstall log file
+        /// Provides a reference to an instance of an uninstall log file
         /// </summary>
         private static Logfile UninstallLogfile;
+
         private static Logfile PatcherLogfile;
+
         private static Logfile EditorLogfile;
+
         private static Logfile UpdaterLogfile;
+
         private static bool FailedToWriteToLogWindowShown = false;
-        public static event LoggingUIThreadReport OnLoggingUIThreadReport;
+
         /// <summary>
-        /// Initialize the logging subsystem for the application
+        /// Event for subscribing as a callback event for when the logfile writes
         /// </summary>
+        public static event LoggingUIThreadReport OnLoggingUIThreadReport;
+
+        /// <summary>
+        /// Initialize the logging system for the application
+        /// </summary>
+        /// <param name="logfile">The log file to initialize</param>
+        /// <param name="logfilePath">The custom path of where to initialize the file</param>
         /// <returns>True if successful initialization, false otherwise</returns>
         public static bool Init(Logfiles logfile, string logfilePath = null)
         {
@@ -179,6 +236,11 @@ namespace RelhaxModpack
             return true;
         }
 
+        /// <summary>
+        /// Checks if the logfile is disposed
+        /// </summary>
+        /// <param name="file">The logfile to check</param>
+        /// <returns>True if the logfile object is null</returns>
         public static bool IsLogDisposed(Logfiles file)
         {
             switch (file)
@@ -199,6 +261,11 @@ namespace RelhaxModpack
             }
         }
 
+        /// <summary>
+        /// Checks if the logfile is instanced and can be written to using the CanWrite property
+        /// </summary>
+        /// <param name="file">The logfile to check</param>
+        /// <returns>True if the logfile is open and available to be written to</returns>
         public static bool IsLogOpen(Logfiles file)
         {
             switch (file)
@@ -235,9 +302,11 @@ namespace RelhaxModpack
                     return ApplicationLogfile.CanWrite;
             }
         }
+
         /// <summary>
         /// Dispose of the application logging subsystem
         /// </summary>
+        /// <param name="logfile">The logfile to dispose</param>
         public static void DisposeLogging(Logfiles logfile)
         {
             switch (logfile)
@@ -269,6 +338,10 @@ namespace RelhaxModpack
             }
         }
 
+        /// <summary>
+        /// Writes the ApplicationlogStartStop constant to the logfile
+        /// </summary>
+        /// <param name="logfile">The logfile to write the header on</param>
         public static void WriteHeader(Logfiles logfile)
         {
             switch(logfile)
@@ -361,106 +434,209 @@ namespace RelhaxModpack
             }
         }
 
+        /// <summary>
+        /// Writes a message to a logfile instance, if it exists
+        /// </summary>
+        /// <param name="messageFormat">The formatted string to be passed into the string.Format() method</param>
+        /// <param name="logfile">The logfile to write to</param>
+        /// <param name="level">The severity level of the message. Will be written as part of the format</param>
+        /// <param name="args">The arguments to be passed into the string.Format() method</param>
         public static void WriteToLog(string messageFormat, Logfiles logfile, LogLevel level, params object[] args)
         {
             WriteToLog(string.Format(messageFormat, args),logfile,level);
         }
 
+        /// <summary>
+        /// Writes a debug level message to the application log
+        /// </summary>
+        /// <param name="message">The message</param>
         public static void Debug(string message)
         {
             WriteToLog(message, Logfiles.Application, LogLevel.Debug);
         }
 
+        /// <summary>
+        /// Writes a debug level message to the application log
+        /// </summary>
+        /// <param name="message">The formatted string to be passed into the string.Format() method</param>
+        /// <param name="args">The arguments to be passed into the string.Format() method</param>
         public static void Debug(string message, params object[] args)
         {
             WriteToLog(message, Logfiles.Application, LogLevel.Debug, args);
         }
 
+        /// <summary>
+        /// Writes a information (info) level message to the application log
+        /// </summary>
+        /// <param name="message">The message</param>
         public static void Info(string message)
         {
             WriteToLog(message, Logfiles.Application, LogLevel.Info);
         }
 
+        /// <summary>
+        /// Writes an information (info) level message to the application log
+        /// </summary>
+        /// <param name="message">The formatted string to be passed into the string.Format() method</param>
+        /// <param name="args">The arguments to be passed into the string.Format() method</param>
         public static void Info(string message, params object[] args)
         {
             WriteToLog(message, Logfiles.Application, LogLevel.Info, args);
         }
 
+        /// <summary>
+        /// Writes a warning level message to the application log
+        /// </summary>
+        /// <param name="message">The message</param>
         public static void Warning(string message)
         {
             WriteToLog(message, Logfiles.Application, LogLevel.Warning);
         }
 
+        /// <summary>
+        /// Writes a warning level message to the application log
+        /// </summary>
+        /// <param name="message">The formatted string to be passed into the string.Format() method</param>
+        /// <param name="args">The arguments to be passed into the string.Format() method</param>
         public static void Warning(string message, params object[] args)
         {
             WriteToLog(message, Logfiles.Application, LogLevel.Warning, args);
         }
 
+        /// <summary>
+        /// Writes an error level message to the application log
+        /// </summary>
+        /// <param name="message">The message</param>
         public static void Error(string message)
         {
             WriteToLog(message, Logfiles.Application, LogLevel.Error);
         }
 
+        /// <summary>
+        /// Writes a error level message to the application log
+        /// </summary>
+        /// <param name="message">The formatted string to be passed into the string.Format() method</param>
+        /// <param name="args">The arguments to be passed into the string.Format() method</param>
         public static void Error(string message, params object[] args)
         {
             WriteToLog(message, Logfiles.Application, LogLevel.Error, args);
         }
 
+        /// <summary>
+        /// Writes an exception level message to the application log
+        /// </summary>
+        /// <param name="message">The message</param>
         public static void Exception(string message)
         {
             WriteToLog(message, Logfiles.Application, LogLevel.Error);
         }
 
+        /// <summary>
+        /// Writes an exception level message to the application log
+        /// </summary>
+        /// <param name="message">The formatted string to be passed into the string.Format() method</param>
+        /// <param name="args">The arguments to be passed into the string.Format() method</param>
         public static void Exception(string message, params object[] args)
         {
             WriteToLog(message, Logfiles.Application, LogLevel.Exception, args);
         }
 
+        /// <summary>
+        /// Writes a message to the Installer log
+        /// </summary>
+        /// <param name="message">The message</param>
         public static void Installer(string message)
         {
             InstallLogfile.Write(message);
         }
 
+        /// <summary>
+        /// Writes a message to the Installer log
+        /// </summary>
+        /// <param name="message">The formatted string to be passed into the string.Format() method</param>
+        /// <param name="args">The arguments to be passed into the string.Format() method</param>
         public static void Installer(string message, params object[] args)
         {
             InstallLogfile.Write(string.Format(message, args));
         }
 
+        /// <summary>
+        /// Writes a message to the Uninstaller log
+        /// </summary>
+        /// <param name="message">The message</param>
         public static void Uninstaller(string message)
         {
             InstallLogfile.Write(message);
         }
 
+        /// <summary>
+        /// Writes a message to the Uninstaller log
+        /// </summary>
+        /// <param name="message">The formatted string to be passed into the string.Format() method</param>
+        /// <param name="args">The arguments to be passed into the string.Format() method</param>
         public static void Uninstaller(string message, params object[] args)
         {
             InstallLogfile.Write(string.Format(message, args));
         }
 
+        /// <summary>
+        /// Writes a message to the Editor logfile
+        /// </summary>
+        /// <param name="message">The message</param>
+        /// <param name="level">The level of severity included into the string format</param>
         public static void Editor(string message, LogLevel level = LogLevel.Info)
         {
             WriteToLog(message, Logfiles.Editor, level);
         }
 
+        /// <summary>
+        /// Writes a message to the Editor logfile
+        /// </summary>
+        /// <param name="message">The formatted string to be passed into the string.Format() method</param>
+        /// <param name="level">The level of severity included into the string format</param>
+        /// <param name="args">The arguments to be passed into the string.Format() method</param>
         public static void Editor(string message, LogLevel level = LogLevel.Info, params object[] args)
         {
             WriteToLog(message, Logfiles.Editor, level, args);
         }
 
+        /// <summary>
+        /// Writes a message to the Updater logfile
+        /// </summary>
+        /// <param name="message">The message</param>
+        /// <param name="level">The level of severity included into the string format</param>
         public static void Updater(string message, LogLevel level = LogLevel.Info)
         {
             WriteToLog(message, Logfiles.Updater, level);
         }
 
+        /// <summary>
+        /// Writes a message to the Updater logfile
+        /// </summary>
+        /// <param name="message">The formatted string to be passed into the string.Format() method</param>
+        /// <param name="level">The level of severity included into the string format</param>
+        /// <param name="args">The arguments to be passed into the string.Format() method</param>
         public static void Updater(string message, LogLevel level = LogLevel.Info, params object[] args)
         {
             WriteToLog(message, Logfiles.Updater, level, args);
         }
 
+        /// <summary>
+        /// Writes a message to the Patcher logfile
+        /// </summary>
+        /// <param name="message">The message</param>
+        /// <param name="level">The level of severity included into the string format</param>
         public static void Patcher(string message, LogLevel level = LogLevel.Info)
         {
             WriteToLog(message, Logfiles.Patcher, level);
         }
 
+        /// <summary>
+        /// Writes a message to the Patcher logfile
+        /// </summary>
+        /// <param name="message">The formatted string to be passed into the string.Format() method</param>
+        /// <param name="level">The level of severity included into the string format</param>
+        /// <param name="args">The arguments to be passed into the string.Format() method</param>
         public static void Patcher(string message, LogLevel level = LogLevel.Info, params object[] args)
         {
             WriteToLog(message, Logfiles.Patcher, level, args);
