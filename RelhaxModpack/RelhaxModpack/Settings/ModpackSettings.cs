2ca977b9f451a6c17f5bf21e7bc1eb5d5e153ca0
Willster419 <willster419@outlook.com>
2019-07-31 21:39:36 -0700

    Documented ModpackSettings

--- RelhaxModpack/RelhaxModpack/Settings/ModpackSettings.cs
+++ RelhaxModpack/RelhaxModpack/Settings/ModpackSettings.cs
@@ -20,34 +20,23 @@ namespace RelhaxModpack
         /// OMC style
         /// </summary>
         Legacy = 1,
+
         /// <summary>
         /// Default WPF V2 style
         /// </summary>
         DefaultV2 = 0
     };
-    /// <summary>
-    /// The different loading Gifs for when loading the preview
-    /// </summary>
-    public enum LoadingGifs
-    {
-        /// <summary>
-        /// The standard loading Gif
-        /// </summary>
-        Standard = 0,
-        /// <summary>
-        /// Thirdguards head loading Gif
-        /// </summary>
-        ThirdGuards = 1
-    };
+
     /// <summary>
     /// The types on uninstall mods the application supports
     /// </summary>
     public enum UninstallModes
     {
         /// <summary>
-        /// Default uninstall method to uninstall all modifiecation done by the application
+        /// Default uninstall method to uninstall all modifications done by the application
         /// </summary>
         Default = 0,
+
         /// <summary>
         /// Quick uninstall method to remove 
         /// </summary>
@@ -62,197 +51,286 @@ namespace RelhaxModpack
         /// The stable public database
         /// </summary>
         Stable,
+
         /// <summary>
         /// The unstable public beta database
         /// </summary>
         Beta,
+
         /// <summary>
         /// The unstable private testing database.
         /// </summary>
         Test
     }
+
     /// <summary>
     /// Provides access to all settings used in the modpack.
     /// </summary>
     public static class ModpackSettings
     {
-        //list of properties to exclude from the properties enumeration
+        /// <summary>
+        /// Contains a list of properties that may be defined in the ModpackSettings class, but to exclude from the properties serialization (loading/saving)
+        /// </summary>
         public static readonly string[] PropertiesToExclude = new string[]
         {
             //put blacklist fields here
             nameof(PropertiesToExclude)
         };
-        #region saveable modpack settings
+        #region Save-able modpack settings
         /// <summary>
         /// The custom path to the ModInfo.xml file if loading in test mode
         /// </summary>
         public static string CustomModInfoPath = string.Empty;
 
-        public static string BetaDatabaseSelectedBranch = "master";
         /// <summary>
-        /// toggle if the program should notify the user if the database version is the same as the last installed version
+        /// The name of the git branch to use when downloading the database. Uses the RelhaxModpackDatabase repository
+        /// </summary>
+        /// <remarks>URL for the repository: https://github.com/Willster419/RelhaxModpackDatabase </remarks>
+        public static string BetaDatabaseSelectedBranch = "master";
+
+        /// <summary>
+        /// Toggle if the program should notify the user if the database version is the same as the last installed version
         /// </summary>
         public static bool NotifyIfSameDatabase = false;
+
         /// <summary>
-        /// toggle if the program will backup the current mod installation
+        /// Toggle if the program will backup the current mod installation
         /// </summary>
         public static bool BackupModFolder = false;
+
         /// <summary>
-        /// toggle if the program will clean the mods and res_mods folders before installation
+        /// Toggle if the program will clean the mods and res_mods folders before installation
         /// </summary>
         public static bool CleanInstallation = true;
+
         /// <summary>
-        /// toggle if the program should force the user to manually point to the WoT location
+        /// Toggle if the program should force the user to manually point to the WoT location
         /// </summary>
         public static bool ForceManuel = false;
+
         /// <summary>
-        /// toggle if the application should automatically save the last selected config to also be automatically loaded upon selection load
+        /// Toggle if the application should automatically save the last selected config to also be automatically loaded upon selection load
         /// </summary>
         public static bool SaveLastSelection = false;
+
         /// <summary>
-        /// toggle if the application should save user cache save data like session stats, or auto equip configs
+        /// Toggle if the application should save user cache save data like session stats, or auto equip configs
         /// </summary>
         public static bool SaveUserData = false;
 
+        /// <summary>
+        /// Toggle if the application should save disabled mods in the user's selection file. If selected, when a mod is enabled again, it will be automatically selected
+        /// </summary>
         public static bool SaveDisabledMods = true;
 
+        /// <summary>
+        /// A one time run bool to display a message about how to use user mods
+        /// </summary>
         public static bool DisplayUserModsWarning = true;
 
+        /// <summary>
+        /// Toggle if the application should use debug level logging or just info
+        /// </summary>
         public static bool VerboseLogging = false;
 
-        public static bool AllowStatisticDataGather = true;
         /// <summary>
-        /// toggle for each view if the borders around the child selection options should show
+        /// Toggle if the application can gather statistic data about it's usage
+        /// </summary>
+        /// <remarks>The data that the modpack gathers is only the list of packages you selected for installation. This helps us determine which mods are popular, and for how best to
+        /// prioritize which mods to work on first in the event the support team is overloaded</remarks>
+        public static bool AllowStatisticDataGather = true;
+
+        /// <summary>
+        /// Toggle for each view if the borders around the child selection options should show
         /// </summary>
         public static bool EnableBordersLegacyView = false;
+
         /// <summary>
-        /// toggle for each view if the borders around the child selection options should show
+        /// Toggle for each view if the borders around the child selection options should show
         /// </summary>
         public static bool EnableBordersDefaultV2View = false;
+
         /// <summary>
-        /// toggle for each view if the color change should occur when a child selection happends
+        /// Toggle for each view if the color change should occur when a child selection happens
         /// </summary>
         public static bool EnableColorChangeLegacyView = false;
+
         /// <summary>
-        /// toggle for each view if the color change should occur when a child selection happends
+        /// Toggle for each view if the color change should occur when a child selection happens
         /// </summary>
         public static bool EnableColorChangeDefaultV2View = false;
+
         /// <summary>
-        /// toggle if the installation complete window will be shown
+        /// Toggle if the installation complete window will be shown
         /// </summary>
         public static bool ShowInstallCompleteWindow = false;
+
         /// <summary>
-        /// toggle if the program will delete the WoT appdata cache
+        /// Toggle if the program will delete the WoT appdata cache
         /// </summary>
         public static bool ClearCache = false;
+
         /// <summary>
-        /// toggle if the program will delete xvm, pmod, WoT logs
+        /// Toggle if the program will delete xvm, pmod, WoT logs
         /// </summary>
         public static bool DeleteLogs = false;
+
         /// <summary>
-        /// toggle if the program will create desktop shortcuts
+        /// Toggle if the program will create desktop shortcuts
         /// </summary>
         public static bool CreateShortcuts = false;
+
         /// <summary>
-        /// toggle the ability for the modpack to extract a package as soon as it is downloaded
+        /// Toggle the ability for the modpack to extract a package as soon as it is downloaded
         /// </summary>
         public static bool InstallWhileDownloading = false;
+
         /// <summary>
-        /// toggle the ability to have multiple extractions happening at the same time
+        /// Toggle the ability to have multiple extractions happening at the same time
         /// </summary>
         public static bool MulticoreExtraction = false;
+
         /// <summary>
-        /// toggle export mode
+        /// Toggle export mode
         /// </summary>
         public static bool ExportMode = false;
 
+        /// <summary>
+        /// Force all packages to be enabled in the selection list
+        /// </summary>
         public static bool ForceEnabled = false;
 
+        /// <summary>
+        /// Force all packages to be visible in the selection list
+        /// </summary>
         public static bool ForceVisible = false;
 
+        /// <summary>
+        /// When selected, the installation engine will disable event triggers.
+        /// </summary>
+        /// <remarks>The triggers, when enabled, enable the application to start some install tasks when all required packages are downloaded</remarks>
         public static bool DisableTriggers = true;
 
+        /// <summary>
+        /// Toggle one click install mode
+        /// </summary>
+        /// <remarks>Allows the user to install a selection when clicking the install button</remarks>
         public static bool OneClickInstall = false;
 
+        /// <summary>
+        /// Toggle auto install mode
+        /// </summary>
+        /// <remarks>Allows the user to have the application automatically check for and install a selection file when the database updates</remarks>
         public static bool AutoInstall = false;
+
         /// <summary>
         /// Toggle the advanced installation progress window
         /// </summary>
         public static bool AdvancedInstalProgress = false;
+
         /// <summary>
         /// True - After an installation the application will ask to delete old download cache files
         /// False - After an installation the application will always delete old download cache files
         /// </summary>
         public static bool DeleteCacheFiles = false;
+
         /// <summary>
-        /// toggle for if the Preview window should be shown in fullscreen mode
+        /// Toggle for if the Preview window should be shown in fullscreen mode
         /// </summary>
         public static bool PreviewFullscreen = false;
 
+        /// <summary>
+        /// The time, in a specified unit, to check for anew data base version
+        /// </summary>
         public static int AutoInstallFrequencyInterval = 10;
 
+        /// <summary>
+        /// The time unit to use for the interval (mins, hours, etc)
+        /// </summary>
         public static int AutoInstallFrequencyTimeUnit = 0;
 
         /// <summary>
-        /// the height, in pixels, of the ModSelectionView window
+        /// The height, in pixels, of the ModSelectionView window
         /// </summary>
         public static int ModSelectionHeight = 480;
+
         /// <summary>
-        /// the width, in pixels, of the ModSelectionView window
+        /// The width, in pixels, of the ModSelectionView window
         /// </summary>
         public static int ModSelectionWidth = 800;
+
         /// <summary>
-        /// toggle for if the ModSelectionView window should be shown in fullscreen mode
+        /// Toggle for if the ModSelectionView window should be shown in fullscreen mode
         /// </summary>
         public static bool ModSelectionFullscreen = false;
+
         /// <summary>
-        /// the x-coordinate location, in pixels, of the Preview window
+        /// The x-coordinate location, in pixels, of the Preview window
         /// </summary>
         public static int PreviewX = 0;
+
         /// <summary>
-        /// the y-coordinate location, in pixels, of the Preview window
+        /// The y-coordinate location, in pixels, of the Preview window
         /// </summary>
         public static int PreviewY = 0;
+
         /// <summary>
-        /// the height, in pixels, of the Preview window
+        /// The height, in pixels, of the Preview window
         /// </summary>
         public static int PreviewHeight = 550;
+
         /// <summary>
-        /// the width, in pixels, of the Preview window
+        /// The width, in pixels, of the Preview window
         /// </summary>
         public static int PreviewWidth = 450;
 
+        /// <summary>
+        /// The path to the auto or one click selection file
+        /// </summary>
+        /// <remarks>Used for both auto and one click installation modes</remarks>
         public static string AutoOneclickSelectionFilePath = string.Empty;
 
         /// <summary>
         /// The Uninstall mode to use when uninstalling or installing with the clean install option
         /// </summary>
         public static UninstallModes UninstallMode = UninstallModes.Default;
+
         /// <summary>
         /// The selection view to use
         /// </summary>
         public static SelectionView ModSelectionView = SelectionView.DefaultV2;
+
         /// <summary>
         /// The selected language
         /// </summary>
         public static Languages Language = Languages.English;
+
         /// <summary>
         /// The distribution version of the database to use when selecting mods
         /// </summary>
         public static DatabaseVersions DatabaseDistroVersion = DatabaseVersions.Stable;
+
         /// <summary>
         /// The application distribution version to use. When selected, it won't take affect until application restart
         /// </summary>
         public static ApplicationVersions ApplicationDistroVersion = ApplicationVersions.Stable;
+
         /// <summary>
-        /// toggle for minimizing the application to the system tray (After the application is done installing mods I presume?)
+        /// t=Toggle for minimizing the application to the system tray (After the application is done installing mods I presume?)
         /// </summary>
         public static bool MinimizeToSystemTray = false;
 
+        /// <summary>
+        /// 
+        /// </summary>
         public static double DisplayScale = 0;
         #endregion
 
-        #region legacy compatability
+        #region Legacy compatibility
+        /// <summary>
+        /// Check for old poorly named settings that don't directly match setting property names
+        /// </summary>
+        /// <param name="settings">The node that holds all the settings nodes</param>
         public static void ApplyOldSettings(XmlNodeList settings)
         {
             for (int i = 0; i < settings.Count; i++)
