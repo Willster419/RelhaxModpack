592af0df4fba206e9e5d23de1e7e52bd786f1270
Willster419 <willster419@outlook.com>
2019-07-31 20:52:55 -0700

    Documented EditorSettings

--- RelhaxModpack/RelhaxModpack/Settings/EditorSettings.cs
+++ RelhaxModpack/RelhaxModpack/Settings/EditorSettings.cs
@@ -6,33 +6,80 @@ using System.Threading.Tasks;
 
 namespace RelhaxModpack
 {
+    /// <summary>
+    /// The different ways that the apply and save database buttons can interact
+    /// </summary>
     public enum ApplyBehavior
     {
+        /// <summary>
+        /// Default behavior. The buttons do not interact.
+        /// </summary>
         Default,
+
+        /// <summary>
+        /// When you click the apply button, it also saves the database after, to the default save location.
+        /// </summary>
         ApplyTriggersSave,
+
+        /// <summary>
+        /// When you click the save button, it also clicks the apply button before saving.
+        /// </summary>
         SaveTriggersApply
     }
 
+    /// <summary>
+    /// The settings used in the editor window
+    /// </summary>
     public class EditorSettings
     {
+        /// <summary>
+        /// The user's FTP account username to the bigmods FTP server
+        /// </summary>
         public string BigmodsUsername = string.Empty;
 
+        /// <summary>
+        /// The user's FTP account password to the bigmods FTP server
+        /// </summary>
         public string BigmodsPassword = string.Empty;
 
+        /// <summary>
+        /// Before you click on a new selection to display, it will apply any changes made. Can be used with ApplyBehavior.
+        /// </summary>
         public bool SaveSelectionBeforeLeave = false;
 
+        /// <summary>
+        /// Sort the database list per level. Not currently implemented
+        /// </summary>
         public bool SortDatabaseList = false;
 
+        /// <summary>
+        /// The behavior the editor should use for the save and apply buttons
+        /// </summary>
         public ApplyBehavior ApplyBehavior = ApplyBehavior.Default;
 
+        /// <summary>
+        /// Show a confirmation window when clicking apply
+        /// </summary>
         public bool ShowConfirmationOnPackageApply = true;
 
+        /// <summary>
+        /// Show a confirmation window when clicking to add or move a package
+        /// </summary>
         public bool ShowConfirmationOnPackageAddRemoveMove = true;
 
+        /// <summary>
+        /// The location to save the database file
+        /// </summary>
         public string DefaultEditorSaveLocation = string.Empty;
 
+        /// <summary>
+        /// The timeout, in seconds, until the FTP upload or download window will close. Set to 0 to disable timeout.
+        /// </summary>
         public uint FTPUploadDownloadWindowTimeout = 0;
 
+        /// <summary>
+        /// Create an instance of the EditorSettings class. Settings should be set via property initialization style.
+        /// </summary>
         public EditorSettings() { }
     }
 }
