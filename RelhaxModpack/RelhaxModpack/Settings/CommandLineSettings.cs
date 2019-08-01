103e2b7c96c8603436e8a3680b9df4e559d1f987
Willster419 <willster419@outlook.com>
2019-07-31 20:43:24 -0700

    Documented CommandLineSettings

--- RelhaxModpack/RelhaxModpack/Settings/CommandLineSettings.cs
+++ RelhaxModpack/RelhaxModpack/Settings/CommandLineSettings.cs
@@ -4,34 +4,103 @@ using System.IO;
 
 namespace RelhaxModpack
 {
+    /// <summary>
+    /// The primary functional modes the application can run in
+    /// </summary>
     public enum ApplicationMode
     {
+        /// <summary>
+        /// The default mode of modpack installer. This is the primary focus of the application
+        /// </summary>
         Default,
+
+        /// <summary>
+        /// The database editor mode
+        /// </summary>
         Editor,
+
+        /// <summary>
+        /// The updater mode. Used for updating the database, application, and other various functions
+        /// </summary>
         Updater,
+
+        /// <summary>
+        /// The patch designer mode. Allow the user to create and test patches
+        /// </summary>
         PatchDesigner,
+
+        /// <summary>
+        /// The patch runner mode. Can be used in command line mode, used for patching files given patch file instructions
+        /// </summary>
         Patcher
     }
-    //TODO: documentation
+    
+    /// <summary>
+    /// Handles all parsing and usage of command line arguments
+    /// </summary>
     public static class CommandLineSettings
     {
         //application command line level settings
         //also serves as place to put default values
+        /// <summary>
+        /// Using the application is database test mode. Allows you to test a local database
+        /// </summary>
+        /// <remarks>Activated with "/test"</remarks>
         public static bool TestMode = false;
+
+        /// <summary>
+        /// Skip the application update check
+        /// </summary>
+        /// <remarks>Activated with "/skip-update"</remarks>
         public static bool SkipUpdate = false;
+
+        /// <summary>
+        /// Allows the application (in installer mode) to be launched in a minimized state
+        /// </summary>
+        /// <remarks>Activated with "/silent-start"</remarks>
         public static bool SilentStart = false;
-        //use the filename as check for auto install
+
+        /// <summary>
+        /// The name of the auto install selection file
+        /// </summary>
+        /// <remarks>The application uses the filename as check for if in auto install mode i.e. if the string is not empty.
+        /// The file must be in the "RelhaxUserSelections" folder</remarks>
         public static string AutoInstallFileName = string.Empty;
-        //use key filename as check for update key mode
+
+        /// <summary>
+        /// The name of the file that contains the key for unlocking the updater
+        /// </summary>
+        /// <remarks>use key filename as check for update key mode</remarks>
         public static string UpdateKeyFileName = string.Empty;
+
+        /// <summary>
+        /// The path to load the database from when the application starts
+        /// </summary>
         public static string EditorAutoLoadFileName = string.Empty;
+
+        /// <summary>
+        /// The parsed list of patch instruction files for patch mode
+        /// </summary>
+        /// <remarks>The application will run the patches in order loaded from the command line i.e. left to right</remarks>
         public static List<string> PatchFilenames = new List<string>();
+
+        /// <summary>
+        /// The mode that the application is currently running in
+        /// </summary>
         public static ApplicationMode ApplicationMode = ApplicationMode.Default;
+
+        /// <summary>
+        /// Parse any conflicting command line arguments
+        /// </summary>
         public static void ParseCommandLineConflicts()
         {
             //check for conflicting command line arguments
         }
 
+        /// <summary>
+        /// Parse the command line arguments
+        /// </summary>
+        /// <param name="args">A string array of command line arguments</param>
         public static void ParseCommandLine(string[] args)
         {
             Logging.Info("command line: " + string.Join(" ", args));
