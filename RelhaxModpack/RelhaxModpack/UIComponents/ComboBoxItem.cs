d28720d389615dc265dbb8b6ef0452c345000d07
Willster419 <willster419@outlook.com>
2019-08-02 13:35:20 -0700

    Documented ComboBoxItem

--- RelhaxModpack/RelhaxModpack/UIComponents/ComboBoxItem.cs
+++ RelhaxModpack/RelhaxModpack/UIComponents/ComboBoxItem.cs
@@ -1,15 +1,35 @@
 ﻿namespace RelhaxModpack.UIComponents
 {
-    //an item to add to a ComboBox
-    class ComboBoxItem : System.Windows.Controls.ComboBoxItem
+    /// <summary>
+    /// The ComboBoxItem class is a wrapper class for displaying SelectablePackages as combo box items. The ToString() allows for display of the package name
+    /// </summary>
+    public class ComboBoxItem : System.Windows.Controls.ComboBoxItem
     {
+        /// <summary>
+        /// The SelectablePackage object that is being wrapped around
+        /// </summary>
         public SelectablePackage Package { get; set; }
+
+        /// <summary>
+        /// The text to display in the Combobox
+        /// </summary>
         public string DisplayName { get; set; }
+
+        /// <summary>
+        /// Creates an instance of the ComboBoxItem class
+        /// </summary>
+        /// <param name="package">The package to wrap around</param>
+        /// <param name="display">The text to display in the Combobox</param>
         public ComboBoxItem(SelectablePackage package, string display)
         {
             Package = package;
             DisplayName = display;
         }
+
+        /// <summary>
+        /// Allows for displaying of custom text in the Combobox
+        /// </summary>
+        /// <returns>The text to display in the Combobox (DisplayName property)</returns>
         public override string ToString()
         {
             return DisplayName;
