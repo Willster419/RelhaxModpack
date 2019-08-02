bf41b2146950ac82325b7baca3ee9be7591cbe0d
Willster419 <willster419@outlook.com>
2019-08-02 12:23:15 -0700

    Documented Media

--- RelhaxModpack/RelhaxModpack/DatabaseComponents/Media.cs
+++ RelhaxModpack/RelhaxModpack/DatabaseComponents/Media.cs
@@ -1,27 +1,56 @@
 ﻿namespace RelhaxModpack
 {
+    /// <summary>
+    /// The supported types of media formats supported for preview in the application
+    /// </summary>
     public enum MediaType
     {
-
+        /// <summary>
+        /// Catch-all case for unknown media when parsing
+        /// </summary>
         Unknown = 0,
 
+        /// <summary>
+        /// A direct link to a picture
+        /// </summary>
         Picture = 1,
 
+        /// <summary>
+        /// A direct link to a website (like for embedding a web player)
+        /// </summary>
         Webpage = 2,
 
+        /// <summary>
+        /// A direct link to an audio file
+        /// </summary>
         MediaFile = 3,
 
+        /// <summary>
+        /// Raw HTML code to display in embedded browser
+        /// </summary>
         HTML = 4
     }
+
+    /// <summary>
+    /// A media object is a preview-able component stored in a list in SelectablePackages
+    /// </summary>
     public class Media
     {
-        //SAMPLE YOUTUBE URL: https://www.youtube.com/v/ZwY2E0hjGuU?version=3&autoplay=1
-        //http and https both work
+        /// <summary>
+        /// For direct link types, the URL to the element or resource
+        /// </summary>
+        /// <remarks>HTTP and HTTPS links work for this property</remarks>
         public string URL = "";
 
-        //media type
+        /// <summary>
+        /// The type of media for the URL to be interpreted as
+        /// </summary>
         public MediaType MediaType = MediaType.Picture;
 
+        /// <summary>
+        /// The string properties of the object
+        /// </summary>
+        /// <returns>The integer code of the MediaType and the first 80 characters of the URL</returns>
         public override string ToString()
         {
             if(URL.Length > 79)
