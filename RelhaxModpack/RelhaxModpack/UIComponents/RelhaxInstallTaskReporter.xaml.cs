2b8bcf75da115645ce072b0548c748bcfbe388a6
Willster419 <willster419@outlook.com>
2019-08-02 14:16:13 -0700

    Documented RelhaxInstallTaskReporter

--- RelhaxModpack/RelhaxModpack/UIComponents/RelhaxInstallTaskReporter.xaml.cs
+++ RelhaxModpack/RelhaxModpack/UIComponents/RelhaxInstallTaskReporter.xaml.cs
@@ -16,11 +16,29 @@ using System.Windows.Shapes;
 
 namespace RelhaxModpack.UIComponents
 {
+    /// <summary>
+    /// States of the Task reporter in display to the UI
+    /// </summary>
     public enum TaskReportState
     {
+        /// <summary>
+        /// Not used for this installation
+        /// </summary>
         Inactive,
+
+        /// <summary>
+        /// In use for this installation
+        /// </summary>
         Active,
+
+        /// <summary>
+        /// In use for this installation and the task completed
+        /// </summary>
         Complete,
+
+        /// <summary>
+        /// An error has occurred on that task
+        /// </summary>
         Error
     }
 
@@ -29,6 +47,9 @@ namespace RelhaxModpack.UIComponents
     /// </summary>
     public partial class RelhaxInstallTaskReporter : UserControl, INotifyPropertyChanged
     {
+        /// <summary>
+        /// Create an instance of the RelhaxInstallTaskReporter and init the UI side of it
+        /// </summary>
         public RelhaxInstallTaskReporter()
         {
             InitializeComponent();
@@ -37,6 +58,10 @@ namespace RelhaxModpack.UIComponents
         #region Properties
 
         private TaskReportState _reportState = TaskReportState.Inactive;
+
+        /// <summary>
+        /// Controls the UI state of the thread reporter
+        /// </summary>
         public TaskReportState ReportState
         {
             get
@@ -64,6 +89,13 @@ namespace RelhaxModpack.UIComponents
         }
 
         private bool _isSubProgressActive = false;
+
+        /// <summary>
+        /// Toggle if the second progress bar should be visible
+        /// </summary>
+        /// <remarks>Some tasks (like zip file extraction) have "sub-tasks" that take enough time where tracking their progress is warranted.
+        /// For example, a zip file has many files to extract. That's the main task. However, each file has bytes to extract. If the file to extract
+        /// is large, a subtask to track the extraction progress could be useful</remarks>
         public bool IsSubProgressActive
         {
             get { return _isSubProgressActive; }
@@ -75,6 +107,10 @@ namespace RelhaxModpack.UIComponents
         }
 
         private string _taskTitle = string.Empty;
+
+        /// <summary>
+        /// The name of this task to display
+        /// </summary>
         public string TaskTitle
         {
             get { return _taskTitle; }
@@ -86,6 +122,10 @@ namespace RelhaxModpack.UIComponents
         }
 
         private string _taskText = string.Empty;
+
+        /// <summary>
+        /// The main reporting description of this task. Supports string formatting
+        /// </summary>
         public string TaskText
         {
             get { return _taskText; }
@@ -97,6 +137,9 @@ namespace RelhaxModpack.UIComponents
         }
 
         private int _taskMinimum = 0;
+        /// <summary>
+        /// The minimum value for the main task progress bar
+        /// </summary>
         public int TaskMinimum
         {
             get { return _taskMinimum; }
@@ -108,6 +151,11 @@ namespace RelhaxModpack.UIComponents
         }
 
         private int _taskMaximum = 0;
+
+        /// <summary>
+        /// The maximum value for the main task progress bar
+        /// </summary>
+        /// <remarks>This value can be changed for, example, the number of patches to run</remarks>
         public int TaskMaximum
         {
             get { return _taskMaximum; }
@@ -119,6 +167,10 @@ namespace RelhaxModpack.UIComponents
         }
 
         private int _taskValue = 0;
+
+        /// <summary>
+        /// The current progress of this task. Value must be between Maximum and Minimum.
+        /// </summary>
         public int TaskValue
         {
             get { return _taskValue; }
@@ -129,7 +181,11 @@ namespace RelhaxModpack.UIComponents
             }
         }
 
-        public int _subTaskMininum = 0;
+        private int _subTaskMininum = 0;
+
+        /// <summary>
+        /// The minimum value for the subtask progress bar
+        /// </summary>
         public int SubTaskMinimum
         {
             get { return _subTaskMininum; }
@@ -141,6 +197,10 @@ namespace RelhaxModpack.UIComponents
         }
 
         private int _subTaskMaximum = 100;
+
+        /// <summary>
+        /// The maximum value for the subtask progress bar
+        /// </summary>
         public int SubTaskMaximum
         {
             get { return _subTaskMaximum; }
@@ -152,6 +212,10 @@ namespace RelhaxModpack.UIComponents
         }
 
         private int _subTaskValue = 0;
+
+        /// <summary>
+        /// The current value of the subtask progress. Must be between the maximum and minimum
+        /// </summary>
         public int SubTaskValue
         {
             get { return _subTaskValue; }
@@ -165,8 +229,15 @@ namespace RelhaxModpack.UIComponents
 
         #region Property changed code
         //https://stackoverflow.com/questions/34651123/wpf-binding-a-background-color-initializes-but-not-updating
+        /// <summary>
+        /// Event to trigger when an internal property is changed. It forces a UI update
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
+        /// <summary>
+        /// Method to invoke the PropertyChanged event to update the UI
+        /// </summary>
+        /// <param name="propertyName">The name of the property that changed, to update it's UI binding</param>
         protected void OnPropertyChanged(string propertyName)
         {
             var handle = PropertyChanged;
