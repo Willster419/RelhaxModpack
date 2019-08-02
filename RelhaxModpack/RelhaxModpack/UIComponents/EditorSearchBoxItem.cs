eb1c123a26e5674d38b0ac4c5584346d3e4f8f38
Willster419 <willster419@outlook.com>
2019-08-02 13:38:48 -0700

    Documented EditorSearchBoxItem

--- RelhaxModpack/RelhaxModpack/UIComponents/EditorSearchBoxItem.cs
+++ RelhaxModpack/RelhaxModpack/UIComponents/EditorSearchBoxItem.cs
@@ -1,20 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RelhaxModpack.UIComponents
+﻿namespace RelhaxModpack.UIComponents
 {
+    /// <summary>
+    /// The EditorSearchBoxItem class is a wrapper class for displaying DatabaseObjects as combo box items. The ToString() allows for display of the internal PackageName property
+    /// </summary>
     public class EditorSearchBoxItem : System.Windows.Controls.ComboBoxItem
     {
+        /// <summary>
+        /// The DatabasePackage object that is being wrapped around
+        /// </summary>
         public DatabasePackage Package { get; set; }
+
+        /// <summary>
+        /// The text to display in the Combobox
+        /// </summary>
         public string DisplayName { get; set; }
+
+        /// <summary>
+        /// Creates an instance of the EditorSearchBoxItem class
+        /// </summary>
+        /// <param name="package">The package to wrap around</param>
+        /// <param name="display">The text to display in the Combobox</param>
         public EditorSearchBoxItem(DatabasePackage package, string display)
         {
             Package = package;
             DisplayName = display;
         }
+
+        /// <summary>
+        /// Allows for displaying of custom text in the Combobox
+        /// </summary>
+        /// <returns>The text to display in the Combobox (DisplayName property)</returns>
         public override string ToString()
         {
             return DisplayName;
