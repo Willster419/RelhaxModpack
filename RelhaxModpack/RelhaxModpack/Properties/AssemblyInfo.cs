53c470a8d5e58da0c1f6fb5f6a2568fbcb789447
Willster419 <willster419@outlook.com>
2019-08-30 15:30:43 -0700

    Application version 0.29.0.13 (2019-08-30)

--- RelhaxModpack/RelhaxModpack/Properties/AssemblyInfo.cs
+++ RelhaxModpack/RelhaxModpack/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.29.0.11")]
-[assembly: AssemblyFileVersion("0.29.0.11")]
+[assembly: AssemblyVersion("0.29.0.13")]
+[assembly: AssemblyFileVersion("0.29.0.13")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
