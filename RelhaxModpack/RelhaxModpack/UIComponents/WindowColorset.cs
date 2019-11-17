06659e3e9c72aeb70249414db6d65a59f1bbc294
Willster419 <willster419@outlook.com>
2019-11-17 11:39:05 -0700

    Completed documentation of UI theme engine

--- RelhaxModpack/RelhaxModpack/UIComponents/WindowColorset.cs
+++ RelhaxModpack/RelhaxModpack/UIComponents/WindowColorset.cs
@@ -6,14 +6,29 @@ using System.Threading.Tasks;
 
 namespace RelhaxModpack.UIComponents
 {
+    /// <summary>
+    /// Contains all color definitions to apply for that window, including the list of individual component color definitions
+    /// </summary>
     public class WindowColorset
     {
+        /// <summary>
+        /// The type object for the type of window this colorset belongs to
+        /// </summary>
         public Type WindowType { get; set; } = null;
 
+        /// <summary>
+        /// The background color of the window
+        /// </summary>
         public CustomBrush BackgroundBrush { get; set; } = null;
 
+        /// <summary>
+        /// A list of custom colors to apply for individual UI components
+        /// </summary>
         public Dictionary<string, ComponentColorset> ComponentColorsets { get; set; } = null;
 
+        /// <summary>
+        /// Flag for if this window colorset has had it's original (default) theme backed up
+        /// </summary>
         public bool ColorsetBackedUp { get; set; } = false;
     }
 }
