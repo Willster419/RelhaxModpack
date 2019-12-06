76fe901ebc7d8a0ba82369c94e3bbcb52c897c1f
Willster419 <willster419@outlook.com>
2019-12-06 15:53:08 -0700

    removed chromium stuff

--- RelhaxWPFConvert/RelhaxWPFConvert/App.xaml.cs
+++ RelhaxWPFConvert/RelhaxWPFConvert/App.xaml.cs
@@ -1,13 +1,8 @@
-﻿using CefSharp;
-using CefSharp.Wpf;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
-using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Windows;
 
 namespace RelhaxWPFConvert
@@ -17,50 +12,5 @@ namespace RelhaxWPFConvert
     /// </summary>
     public partial class App : Application
     {
-
-        public App()
-        {
-            //https://stackoverflow.com/questions/4001547/c-sharp-wpf-webbrowser-alternative
-            //https://github.com/cefsharp/CefSharp/issues/1714
-            //Add Custom assembly resolver
-            AppDomain.CurrentDomain.AssemblyResolve += Resolver;
-
-            //Any CefSharp references have to be in another method with NonInlining
-            // attribute so the assembly rolver has time to do it's thing.
-            InitializeCefSharp();
-        }
-
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        private static void InitializeCefSharp()
-        {
-            var settings = new CefSettings();
-
-            // Set BrowserSubProcessPath based on app bitness at runtime
-            settings.BrowserSubprocessPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
-                                                   Environment.Is64BitProcess ? "x64" : "x86",
-                                                   "CefSharp.BrowserSubprocess.exe");
-
-            // Make sure you set performDependencyCheck false
-            Cef.Initialize(settings, performDependencyCheck: false, browserProcessHandler: null);
-        }
-
-        // Will attempt to load missing assembly from either x86 or x64 subdir
-        // Required by CefSharp to load the unmanaged dependencies when running using AnyCPU
-        private static Assembly Resolver(object sender, ResolveEventArgs args)
-        {
-            if (args.Name.StartsWith("CefSharp"))
-            {
-                string assemblyName = args.Name.Split(new[] { ',' }, 2)[0] + ".dll";
-                string archSpecificPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
-                                                       Environment.Is64BitProcess ? "x64" : "x86",
-                                                       assemblyName);
-
-                return File.Exists(archSpecificPath)
-                           ? Assembly.LoadFile(archSpecificPath)
-                           : null;
-            }
-
-            return null;
-        }
     }
 }
--- RelhaxWPFConvert/RelhaxWPFConvert/MainWindow.xaml.cs
+++ RelhaxWPFConvert/RelhaxWPFConvert/MainWindow.xaml.cs
@@ -32,7 +32,6 @@ using Microsoft.WindowsAPICodePack.Taskbar;
 using System.Xml.Linq;
 using HtmlAgilityPack;
 using System.Text;
-using CefSharp;
 using Microsoft.Win32;
 
 namespace RelhaxWPFConvert
@@ -610,59 +609,6 @@ namespace RelhaxWPFConvert
                     taskbarInstance.SetProgressValue(result, 100);
                 }
             }
-        }
-
-        private async void AutoUpdateWGClick_Click(object sender, RoutedEventArgs e)
-        {
-            if (string.IsNullOrWhiteSpace(AutoUpdateWGURLTextbox.Text))
-                return;
-            AutoUpdateWGInfo.Text = "Getting info...";
-
-            int framesLoaded = 0;
-            bool browserLoaded = false;
-            CefBrowser.FrameLoadEnd += (sendahh, endArgs) =>
-            {
-                framesLoaded++;
-            };
-            CefBrowser.LoadingStateChanged += (send, args) =>
-            {
-                if (!args.IsLoading)
-                    browserLoaded = true;
-            };
-
-            CefBrowser.Load(AutoUpdateWGURLTextbox.Text);
-
-            while (!browserLoaded)
-                await Task.Delay(500);
-
-            while (CefBrowser.IsLoading)
-                await Task.Delay(500);
-
-            while (framesLoaded < 2)
-                await Task.Delay(500);
-
-            string s = await CefBrowser.GetSourceAsync();
-
-            //http://blog.olussier.net/2010/03/30/easily-parse-html-documents-in-csharp/
-            
-            HtmlDocument document = new HtmlDocument();
-            document.LoadHtml(s);
-            HtmlNode node = document.DocumentNode;
-            //https://stackoverflow.com/questions/1390568/how-can-i-match-on-an-attribute-that-contains-a-certain-string
-            HtmlNodeCollection clientVersionNode = node.SelectNodes(@"//div[contains(@class, 'ModDetails_label')]");
-            string version = string.Empty;
-            if(clientVersionNode != null)
-            {
-                HtmlNode nodeTest = clientVersionNode[3];
-                HtmlNode versionNode = nodeTest.ChildNodes[0].ChildNodes[1];
-                version = versionNode.InnerText;
-            }
-            
-
-            HtmlNode downloadUrlNode = node.SelectSingleNode(@"//a[contains(@class, 'ModDetails_hidden')]");
-            string downloadURL = downloadUrlNode.Attributes["href"].Value;
-
-            AutoUpdateWGInfo.Text = string.Format("For client: {0}, download link: {1}",version,downloadURL);
         }
 
         private async void AutoUpdateWGClickIE_Click(object sender, RoutedEventArgs e)
