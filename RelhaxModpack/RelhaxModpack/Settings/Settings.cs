1be47bfe0e1deb1d5a0f8773025a03c62b5fd16a
Willster419 <willster419@outlook.com>
2020-01-18 15:30:55 -0700

    same with this

--- RelhaxModpack/RelhaxModpack/Settings/Settings.cs
+++ RelhaxModpack/RelhaxModpack/Settings/Settings.cs
@@ -469,9 +469,6 @@ namespace RelhaxModpack
         public static Ionic.Zip.ZipFile ManagerInfoZipfile = null;
         #endregion
 
-        //had to put this here because of bugs with intellisense. It's supposed to be in mod selection list
-#warning using V1 beta database
-
         #region Settings parsing to/from XML file
         /// <summary>
         /// Loads/serializes an xml file into a settings class based on class type
