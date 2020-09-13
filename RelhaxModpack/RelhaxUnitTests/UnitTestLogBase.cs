af4d1afa320c0e9623607073d879a34db1ea8e1a
Willster419 <willster419@outlook.com>
2020-09-13 09:14:11 -0700

    added comments and fixed bug with logfiles without a default name not being initialized with ".log" at the end of the filename

--- RelhaxModpack/RelhaxUnitTests/UnitTestLogBase.cs
+++ RelhaxModpack/RelhaxUnitTests/UnitTestLogBase.cs
@@ -14,8 +14,11 @@ namespace RelhaxUnitTests
             //init all logs if they aren't already init
             foreach (Logfiles logfile in UnitTestHelper.AllLogFiles)
             {
+                //if the log file isn't already open, then create it
                 if(!Logging.IsLogOpen(logfile))
-                    if (!Logging.Init(logfile, UnitTestHelper.LogFilesWithPresetFilenames.Contains(logfile) ? null : logfile.ToString()))
+                    //init with the default name (pass in null to get default), or if no default, the name of the enumeration and ".log"
+                    //throw exception if it fails to create the log file
+                    if (!Logging.Init(logfile, UnitTestHelper.LogFilesWithPresetFilenames.Contains(logfile) ? null : string.Format("{0}.log", logfile.ToString())))
                         throw new BadMemeException("Failed to create a log file");
             }
         }
