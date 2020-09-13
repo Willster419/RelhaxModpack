d22c44276c316a26c93da77d6f2ff4d4505984ba
Willster419 <willster419@outlook.com>
2020-09-13 09:14:28 -0700

    started work on translations unit test

---
+++ RelhaxModpack/RelhaxUnitTests/Set03_TranslationsTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using RelhaxModpack;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RelhaxUnitTests
+{
+    [TestClass]
+    public class Set03_TranslationsTest : UnitTestLogBase
+    {
+        [TestMethod]
+        public void Test01_LoadTranslations()
+        {
+            Assert.IsFalse(Translations.TranslationsLoaded);
+            Translations.LoadTranslations();
+            Assert.IsTrue(Translations.TranslationsLoaded);
+        }
+    }
+}
