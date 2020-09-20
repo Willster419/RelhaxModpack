4a6fe7e27c8abd1463e635095907892f739cd9cb
Willster419 <willster419@outlook.com>
2020-09-20 15:46:13 -0700

    Updated MsTest nuget packages, removed InstallerUnitTestHelper by using private static methods and [ClassInitialize] attribute for unit test classes. Working implementation of testing the ModSelectionList window, complete with button press event injection. Also includes loading a blank selection list. This is the beginning of something beautiful. I can feel it.

--- RelhaxModpack/RelhaxInstallerUnitTester/Set01_ModSelectionListTests.cs
+++ RelhaxModpack/RelhaxInstallerUnitTester/Set01_ModSelectionListTests.cs
@@ -11,61 +11,141 @@ using System.Windows.Controls;
 using RelhaxModpack.UI;
 using System.Windows;
 using System.IO;
+using System.Windows.Threading;
+using System.Threading;
+using RelhaxModpack.Utilities;
 
 namespace RelhaxInstallerUnitTester
 {
     [TestClass]
     public class Set01_ModSelectionListTests : UnitTestLogBase
     {
+        //declaring these objects as static will allow them to exist throughout the test
+        //exists in all methods
+        private static ModSelectionList SelectionList = null;
+        private static List<DatabasePackage> GlobalDependencies = null;
+        private static List<Dependency> Dependencies = null;
+        private static List<Category> ParsedCategoryList = null;
+        private static Logfile log = null;
+        private static App app = null;
+
         [TestMethod]
         public void Test01_LoadModpackSettingsTest()
         {
-            InstallerUnitTestHelper.InstallerHelperLog = UnitTestHelper.CreateLogfile();
-            Assert.IsNotNull(InstallerUnitTestHelper.InstallerHelperLog);
-            Assert.IsTrue(InstallerUnitTestHelper.InstallerHelperLog.CanWrite);
-
             bool settingsLoaded = Settings.LoadSettings(Settings.ModpackSettingsFileName, typeof(ModpackSettings), null, null);
             if (File.Exists(Settings.ModpackSettingsFileName))
                 Assert.IsTrue(settingsLoaded);
+
+            GlobalDependencies = new List<DatabasePackage>();
         }
 
         [TestMethod]
-        public void Test02_LoadTranslationsForSelectionListTest()
+        public async Task Test02_LoadMiscForSelectionListTest()
         {
-            InstallerUnitTestHelper.InstallerHelperLog.Write("Loading translations for windows");
+            log.Write("Loading translations for windows");
             Translations.LoadTranslations();
+            Assert.IsTrue(Translations.TranslationsLoaded);
+
+            //set properties outside of the selection list
+            log.Write("Setting all required misc settings");
+            ModpackSettings.DatabaseDistroVersion = RelhaxModpack.Utilities.Enums.DatabaseVersions.Beta;
+            ModpackSettings.SaveLastSelection = true;
+
+            //ensure folder structure exists
+            log.Write("Ensuring folder structure exists");
+            foreach(string folderPath in Settings.FoldersToCheck)
+            {
+                if (!Directory.Exists(folderPath))
+                    Directory.CreateDirectory(folderPath);
+            }
+
+            //get the managerInfo document
+            Settings.ManagerInfoZipfile = await CommonUtils.GetManagerInfoZipfileAsync(true);
         }
 
         [TestMethod]
         public async Task Test03_LoadModSelectionListTest()
         {
-            InstallerUnitTestHelper.InstallerHelperLog.Write("Creating a ModSelectionList Window");
-            InstallerUnitTestHelper.InstallerHelperList = new ModSelectionList()
+            app.InitializeComponent();
+            log.Write("Creating a ModSelectionList Window");
+
+            //create the window and run it on its own thread and dispatcher
+            //this can avoid problems with the unit test dispatcher not running the window the way it should
+            //it's still an STA thread so WPF can use it as a UI thread
+            //https://www.c-sharpcorner.com/uploadfile/suchit_84/creating-wpf-windows-on-dedicated-threads/
+            Thread thread = new Thread(() =>
             {
-                ApplyColorSettings = false,
-                ApplyScaling = false,
-                ApplyToolTips = true,
-                AutoInstallMode = false,
-                LocalizeWindow = true,
-                OriginalHeight = 720.0,
-                OriginalWidth = 1280.0,
-                LastSupportedWoTClientVersion = "1.10.0.1",
-                //the lists are newed in the application
-                GlobalDependencies = InstallerUnitTestHelper.GlobalDependencies,
-                Dependencies = InstallerUnitTestHelper.Dependencies,
-                ParsedCategoryList = InstallerUnitTestHelper.ParsedCategoryList
-            };
+                SelectionList = new ModSelectionList()
+                {
+                    ApplyColorSettings = false,
+                    ApplyScaling = false,
+                    ApplyToolTips = true,
+                    AutoInstallMode = false,
+                    LocalizeWindow = true,
+                    OriginalHeight = 720.0,
+                    OriginalWidth = 1280.0,
+                    LastSupportedWoTClientVersion = "1.10.0.2",
+                    //the lists are newed in the application
+                    GlobalDependencies = Set01_ModSelectionListTests.GlobalDependencies,
+                    Dependencies = Set01_ModSelectionListTests.Dependencies,
+                    ParsedCategoryList = Set01_ModSelectionListTests.ParsedCategoryList
+                };
 
-            InstallerUnitTestHelper.InstallerHelperList.Show();
+                SelectionList.Closed += (sender, e) => SelectionList.Dispatcher.InvokeShutdown();
+                SelectionList.WindowState = WindowState.Normal;
+                SelectionList.Show();
 
-            await Task.Delay(10000);
+                //start the windows message pump
+                Dispatcher.Run();
 
-            List<FrameworkElement> elements = UiUtils.GetAllWindowComponentsLogical(InstallerUnitTestHelper.InstallerHelperList, false);
+            });
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.IsBackground = true;
+            thread.Start();
 
-            FrameworkElement buttonElement = elements.Find(element => element.Tag != null && element.Tag.Equals("ClearSelectionsButton"));
 
-            Button clearSelectionsButton = buttonElement as Button;
-            clearSelectionsButton.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            while (SelectionList == null)
+                await Task.Delay(100);
+            
+            while (!SelectionList.LoadingUI)
+                await Task.Delay(100);
+
+            while (SelectionList.LoadingUI)
+                await Task.Delay(1000);
+
+            List<FrameworkElement> elements = null;
+            FrameworkElement buttonElement = null;
+
+            SelectionList.Dispatcher.Invoke(() =>
+            {
+                elements = UiUtils.GetAllWindowComponentsLogical(SelectionList, false);
+                buttonElement = elements.Find(element => element.Tag != null && element.Tag.Equals("CancelButton"));
+                Button clearSelectionsButton = buttonElement as Button;
+                clearSelectionsButton.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            });
+        }
+
+        [ClassInitialize]
+        public static void InitClass(TestContext ctx)
+        {
+            string[] fullyQualifiedTestNameSplit = ctx.FullyQualifiedTestClassName.Split('.');
+            //throw exception if it fails to create the log file
+            log = new Logfile(fullyQualifiedTestNameSplit[fullyQualifiedTestNameSplit.Length-1], Logging.ApplicationLogfileTimestamp);
+
+            log.Init();
+
+            if (!log.CanWrite)
+                throw new BadMemeException("Can't write to the logfile");
+
+            //init the modpack app to load resources required by the unit tested windows
+            //https://stackoverflow.com/a/20834469/3128017
+            //https://stackoverflow.com/a/39841167/3128017
+            if (Application.Current == null)
+            {
+                new App { ShutdownMode = ShutdownMode.OnExplicitShutdown };
+                (Application.Current as App).InitializeComponent();
+                app = Application.Current as App;
+            }
         }
     }
 }
