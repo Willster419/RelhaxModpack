a1f08450e6c51f3855b8f65d477a3ffc183c6d45
Willster419 <willster419@outlook.com>
2020-11-28 11:19:54 -0700

    and finally finish renaming the project to RelhaxSandbox

--- RelhaxSandbox/RelhaxSandbox/Properties/Resources.Designer.cs
+++ RelhaxSandbox/RelhaxSandbox/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace RelhaxSandbox.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RelhaxWPFConvert.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RelhaxSandbox.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
