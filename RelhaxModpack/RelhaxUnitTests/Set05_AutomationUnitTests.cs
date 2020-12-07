533f5137fedbd34aa0fdb47d286df090d6c1fcfd
Willster419 <willster419@outlook.com>
2020-12-06 22:30:41 -0700

    logs show that the serialization will need an update, and the macro system still needs to be implemented, but a sandbox test works!

--- RelhaxModpack/RelhaxUnitTests/Set05_AutomationUnitTests.cs
+++ RelhaxModpack/RelhaxUnitTests/Set05_AutomationUnitTests.cs
@@ -3,28 +3,54 @@ using RelhaxModpack;
 using RelhaxModpack.Automation;
 using System;
 using System.Threading.Tasks;
+using RelhaxModpack.Utilities.Enums;
+using System.Collections.Generic;
+using RelhaxModpack.Database;
 
 namespace RelhaxUnitTests
 {
     [TestClass]
     public class Set05_AutomationUnitTests : UnitTestLogBase
     {
+        AutomationRunnerSettings AutomationRunnerSettings = new AutomationRunnerSettings()
+        {
+            DumpParsedMacrosPerSequenceRun = true,
+            SelectedBranch = "master"
+        };
+
         [TestMethod]
         public async Task Test01_GetAutomationRepoBranchesTest()
         {
             AutomationSequencer sequencer = new AutomationSequencer()
             {
-                AutomationRunnerSettings = new AutomationRunnerSettings()
-                {
-                    SelectedBranch = "master" // is default
-                }
+                AutomationRunnerSettings = this.AutomationRunnerSettings,
+                AutomationRunMode = AutomationRunMode.Interactive
             };
 
             await sequencer.LoadBranchesListAsync();
             await sequencer.LoadRootDocumentAsync();
             await sequencer.LoadGlobalMacrosAsync();
+            List<DatabasePackage> DatabasePackages = new List<DatabasePackage>();
+            DatabasePackages.Add(new SelectablePackage()
+            { 
+                UID = "123456789ABCD",
+                PackageName = "Some_rofl_op_russian_medium",
+                ParentCategory = new Category() { Name = "Cat_name" },
+                Level = 0
+            });
+            bool loadAutomationSequencesResult = await sequencer.LoadAutomationSequencesAsync(DatabasePackages);
+            bool parseAutomationSequencesResult = sequencer.ParseAutomationSequences();
+            bool runSequencesResult = await sequencer.RunSequencesAsync();
             //Assert.IsFalse(string.IsNullOrEmpty(LatestSupportedWoTVersion));
             //Assert.IsFalse(string.IsNullOrEmpty(LatestSupportedWoTVersion));
+        }
+
+        [TestInitialize]
+        public void SetDefaultValues()
+        {
+            //TODO: dynamically get this from the beta db?
+            Settings.WoTModpackOnlineFolderVersion = "1.10.0";
+            Settings.WoTClientVersion = "1.10.0.4";
         }
     }
 }
