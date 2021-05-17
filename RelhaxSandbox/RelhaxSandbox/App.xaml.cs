10ac95ea6e2d7cedcaf41fac62d5baa609b31713
Willster419 <willster419@outlook.com>
2021-05-17 00:11:37 -0700

    sandbox play to figure out which embedded browser i want to use, and debug

--- RelhaxSandbox/RelhaxSandbox/App.xaml.cs
+++ RelhaxSandbox/RelhaxSandbox/App.xaml.cs
@@ -12,5 +12,9 @@ namespace RelhaxSandbox
     /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.ToString());
+        }
     }
 }
