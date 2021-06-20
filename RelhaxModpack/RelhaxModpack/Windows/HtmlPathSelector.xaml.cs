88463f2c5d8952be04a4091e178da05d422226f2
Willster419 <willster419@outlook.com>
2021-06-19 20:36:00 -0700

    add output of the browser loaded html document to disk to debug. Adjust height of window and components. Set delay slider to snap to ticks. If the url doesn't change, it won't re-load the page, just run the html path search code. Update logging. Add WindowsInterop.cs to use win32 api's to accept SSL and security certificates automatically

--- RelhaxModpack/RelhaxModpack/Windows/HtmlPathSelector.xaml.cs
+++ RelhaxModpack/RelhaxModpack/Windows/HtmlPathSelector.xaml.cs
@@ -1,21 +1,16 @@
-﻿using HtmlAgilityPack;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Xml.XPath;
+using HtmlAgilityPack;
+using RelhaxModpack.Common;
 using RelhaxModpack.Settings;
 using RelhaxModpack.Utilities;
 using RelhaxModpack.Utilities.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using System.Xml.XPath;
+using HtmlDocument = HtmlAgilityPack.HtmlDocument;
 
 namespace RelhaxModpack.Windows
 {
@@ -24,6 +19,8 @@ namespace RelhaxModpack.Windows
     /// </summary>
     public partial class HtmlPathSelector : RelhaxWindow
     {
+
+        public const string HtmlDocumentTextFilename = "HtmlOutput.html";
 
         private bool browserNavigated, browserDocumentCompleted = false;
 
@@ -34,11 +31,32 @@ namespace RelhaxModpack.Windows
         public HtmlPathSelector(ModpackSettings modpackSettings) : base (modpackSettings)
         {
             InitializeComponent();
+
+            WindowsInterop.SecurityAlertDialogWillBeShown += new GenericDelegate<Boolean, Boolean>(this.WindowsInterop_SecurityAlertDialogWillBeShown);
+
+            WindowsInterop.Hook();
         }
 
         private async void UrlGoButton_Click(object sender, RoutedEventArgs e)
         {
             HtmlPathResultsTextBox.Text = "Loading...";
+            
+            if (Browser.Url != null && Browser.Url.Equals(UrlTextBox.Text))
+            {
+                Logging.Info("The URL did not change, we can skip the browser run");
+            }
+            else
+            {
+                Logging.Info("The URL changed or is null, load the browser page");
+                await RunBrowserAsync();
+            }
+
+            RunHtmlPathSearch();
+        }
+
+        private async Task RunBrowserAsync()
+        {
+            //reset browser counts
             browserNavigated = false;
             browserDocumentCompleted = false;
             browserFinishedLoadingScriptsCounter = 0;
@@ -68,15 +86,19 @@ namespace RelhaxModpack.Windows
                 Logging.Debug(Logfiles.AutomationRunner, "Waiting {0} of {1} counts", ++browserFinishedLoadingScriptsCounter, waitCounts);
             }
 
+            //save to string
+            Logging.Info("Writing HTML to {0}", HtmlDocumentTextFilename);
+            File.WriteAllText(HtmlDocumentTextFilename, Browser.Document.Body.OuterHtml);
             Logging.Info(Logfiles.AutomationRunner, "The browser reports all loading done, save html to string");
+        }
 
-            await Task.Delay((int)SetDelaySlider.Value);
-
+        private void RunHtmlPathSearch()
+        {
             HtmlDocument document = new HtmlDocument();
             string htmlText = Browser.Document.Body.OuterHtml;
             document.LoadHtml(htmlText);
-            HtmlNodeNavigator navigator = (HtmlAgilityPack.HtmlNodeNavigator)document.CreateNavigator();
-            HtmlNodeNavigator result = null;
+            HtmlNodeNavigator navigator = (HtmlNodeNavigator)document.CreateNavigator();
+            HtmlNodeNavigator result;
             //https://stackoverflow.com/questions/1390568/how-can-i-match-on-an-attribute-that-contains-a-certain-string
             //sample htmlPath to get download link: @"//a[contains(@class, 'ModDetails_hidden')]//@href"
             //HtmlNodeCollection clientVersionNode = node.SelectNodes(@"//div[contains(@class, 'ModDetails_label')]");
@@ -91,14 +113,18 @@ namespace RelhaxModpack.Windows
                 Logging.Exception(ex.ToString());
                 return;
             }
+
             if (result == null)
             {
                 Logging.Info("Result was not found");
                 HtmlPathResultsTextBox.Text = "Result was not found";
             }
-            Logging.Debug(Logfiles.AutomationRunner, "HtmlPath results in node value '{0}' of type '{1}'", result.InnerXml, result.NodeType.ToString());
-            HtmlPathResultsTextBox.Text = string.Format("Result value as text: {0}\nResult node type: {1}\nResult inner html: {2}\nResult outer html: {3}",
-                result.Value, result.NodeType.ToString(), result.InnerXml, result.OuterXml);
+            else
+            {
+                Logging.Debug(Logfiles.AutomationRunner, "HtmlPath results in node value '{0}' of type '{1}'", result.InnerXml, result.NodeType.ToString());
+                HtmlPathResultsTextBox.Text = string.Format("Result value as text: {0}\nResult node type: {1}\nResult inner html: {2}\nResult outer html: {3}",
+                    result.Value, result.NodeType.ToString(), result.InnerXml, result.OuterXml);
+            }
         }
 
         private void RelhaxWindow_Loaded(object sender, RoutedEventArgs e)
@@ -108,16 +134,15 @@ namespace RelhaxModpack.Windows
             SetDelaySlider_ValueChanged(null, null);
         }
 
-        private async void Browser_DocumentCompleted(object sender, System.Windows.Forms.WebBrowserDocumentCompletedEventArgs e)
+        private void Browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
-            Logging.Debug(Logfiles.AutomationRunner, "The browser reports document completed, wait for timeout");
+            Logging.Debug(Logfiles.AutomationRunner, "The browser reports document completed");
             browserDocumentCompleted = true;
-            
         }
 
-        private void Browser_Navigated(object sender, System.Windows.Forms.WebBrowserNavigatedEventArgs e)
+        private void Browser_Navigated(object sender, WebBrowserNavigatedEventArgs e)
         {
-            Logging.Debug(Logfiles.AutomationRunner, "The browser reports navigation completed, wait for document completed and timeout");
+            Logging.Debug(Logfiles.AutomationRunner, "The browser reports navigation completed");
             browserNavigated = true;
         }
 
@@ -125,6 +150,18 @@ namespace RelhaxModpack.Windows
         {
             if (SetDelayValueTextBlock != null)
                 SetDelayValueTextBlock.Text = string.Format("{0} ms", (int)SetDelaySlider.Value);
+        }
+
+        private Boolean WindowsInterop_SecurityAlertDialogWillBeShown(Boolean blnIsSSLDialog)
+        {
+            // Return true to ignore and not show the 
+            // "Security Alert" dialog to the user
+            return true;
+        }
+
+        private void RelhaxWindow_Closed(object sender, EventArgs e)
+        {
+            WindowsInterop.Unhook();
         }
     }
 }
