9a3bc31b42b603c53f2a0209ce7e900ceb758f3d
Willster419 <willster419@outlook.com>
2021-07-18 19:38:32 -0700

    Beta Application version 1.3.2.20 (2021-07-18)

--- RelhaxModpack/RelhaxModpack/Properties/AssemblyInfo.cs
+++ RelhaxModpack/RelhaxModpack/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.2.18")]
-[assembly: AssemblyFileVersion("1.3.2.18")]
+[assembly: AssemblyVersion("1.3.2.20")]
+[assembly: AssemblyFileVersion("1.3.2.20")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
