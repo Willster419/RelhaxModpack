61d96e2a0c1e90c8ad30442a525b6a78d934313f
Willster419 <willster419@outlook.com>
2021-11-27 19:29:33 -0700

    GetStringValue() can be async for the child classes like the html download

--- RelhaxModpack/RelhaxModpack/Automation/Tasks/MacroStringSplitTask.cs
+++ RelhaxModpack/RelhaxModpack/Automation/Tasks/MacroStringSplitTask.cs
@@ -58,7 +58,7 @@ namespace RelhaxModpack.Automation.Tasks
         public override async Task RunTask()
         {
             await base.RunTask();
-            GetStringValue();
+            await GetStringValue();
 
             if (string.IsNullOrEmpty(stringWithValue))
             {
