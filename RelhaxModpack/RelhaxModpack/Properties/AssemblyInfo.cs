0e6d580e010066fd88b73fa4a9ca284798d51882
Willster419 <willster419@outlook.com>
2022-01-19 14:17:33 -0700

    Application version 1.3.5.0 (2022-01-19)

--- RelhaxModpack/RelhaxModpack/Properties/AssemblyInfo.cs
+++ RelhaxModpack/RelhaxModpack/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Relic Gaming Community")]
 [assembly: AssemblyProduct("Relhax Modpack Installer")]
-[assembly: AssemblyCopyright("Copyright © 2021 Willard Wider")]
+[assembly: AssemblyCopyright("Copyright © 2022 Willard Wider")]
 [assembly: AssemblyTrademark("The fastest WoT modpack installer in the world")]
 [assembly: AssemblyCulture("")]
 
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.4.0")]
-[assembly: AssemblyFileVersion("1.3.4.0")]
+[assembly: AssemblyVersion("1.3.5.0")]
+[assembly: AssemblyFileVersion("1.3.5.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
