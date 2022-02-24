b2fb86f3ca73f8dfd8e211cd6ad6d42c9c925d47
Willster419 <willster419@outlook.com>
2022-02-23 18:55:25 -0700

    documented BadMemeException ayy lmao

--- RelhaxModpack/RelhaxModpack/Common/BadMemeException.cs
+++ RelhaxModpack/RelhaxModpack/Common/BadMemeException.cs
@@ -5,9 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace RelhaxModpack
-{   [Serializable]
+{   
+    /// <summary>
+    /// An exception used mostly for mistakes that could happen during development, 'sanity check' type verification. And also for bad memes.
+    /// </summary>
+    [Serializable]
     public class BadMemeException : Exception
     {
+        /// <summary>
+        /// Throw a bad meme exception.
+        /// </summary>
+        /// <param name="message">The message to tell the developer why his meme is bad.</param>
         public BadMemeException(string message) : base(message)
         {
 
