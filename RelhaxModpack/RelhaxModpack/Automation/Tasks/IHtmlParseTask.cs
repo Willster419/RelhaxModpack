0674aea9de12b076b9907d4ea813bffe1cd342f3
Willster419 <willster419@outlook.com>
2022-03-26 15:48:44 -0700

    documented IHtmlParseTask

--- RelhaxModpack/RelhaxModpack/Automation/Tasks/IHtmlParseTask.cs
+++ RelhaxModpack/RelhaxModpack/Automation/Tasks/IHtmlParseTask.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace RelhaxModpack.Automation.Tasks
 {
+    /// <summary>
+    /// An interface that requires the implementing task to use properties needed for HtmlPath navigation.
+    /// </summary>
+    /// <remarks>HtmlPath navigation is used across multiple abstract parent classes of AutomationTask, and this ensures they all use the same argument names.</remarks>
     public interface IHtmlParseTask
     {
+        /// <summary>
+        /// The HtmlPath argument to use for parsing.
+        /// </summary>
         string HtmlPath { get; set; }
     }
 }
